b547f965624a390769462f351e6f4287bd50216a
zwvista <zwvista@msn.com>
2014-03-20 19:25:33 +0900

    Added and Finished Pata.cpp

--- PuzzlesP/main.cpp
+++ PuzzlesP/main.cpp
@@ -3,6 +3,7 @@
 extern void solve_puz_ParkingLot();
 extern void solve_puz_Parks();
 extern void solve_puz_Parks2();
+extern void solve_puz_Pata();
 extern void solve_puz_pathfind();
 extern void solve_puz_patternpuzzle();
 extern void solve_puz_pegsolitary();
@@ -13,8 +14,9 @@ int main(int argc, char **argv)
 	cout << "a1: Parking Lot" << endl;
 	cout << "a2: Parks" << endl;
 	cout << "a3: Parks2" << endl;
-	cout << "a4: pathfind" << endl;
-	cout << "a5: patternpuzzle" << endl;
+	cout << "a4: Pata" << endl;
+	cout << "a5: pathfind" << endl;
+	cout << "a6: patternpuzzle" << endl;
 	cout << "e: pegsolitary" << endl;
 	cout << "h: Pharaoh" << endl;
 	string str;
@@ -22,8 +24,9 @@ int main(int argc, char **argv)
 	if(str == "a1") solve_puz_ParkingLot();
 	else if(str == "a2") solve_puz_Parks();
 	else if(str == "a3") solve_puz_Parks2();
-	else if(str == "a4") solve_puz_pathfind();
-	else if(str == "a5") solve_puz_patternpuzzle();
+	else if(str == "a4") solve_puz_Pata();
+	else if(str == "a5") solve_puz_pathfind();
+	else if(str == "a6") solve_puz_patternpuzzle();
 	else if(str == "e") solve_puz_pegsolitary();
 	else if(str == "h") solve_puz_Pharaoh();
 
