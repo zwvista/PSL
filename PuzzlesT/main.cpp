0141b5446972f2b9e3c3b16c85de2d2a3ea87426
zwvista <zwvista@msn.com>
2014-03-21 10:15:50 +0900

    Added and Finished TapAlike.cpp

--- PuzzlesT/main.cpp
+++ PuzzlesT/main.cpp
@@ -1,6 +1,7 @@
 #include "stdafx.h"
 
 extern void solve_puz_Tapa();
+extern void solve_puz_TapAlike();
 extern void solve_puz_TapARow();
 extern void solve_puz_TapDifferently();
 extern void solve_puz_tatami();
@@ -12,9 +13,10 @@ extern void solve_puz_twinballs();
 int main(int argc, char **argv)
 {
 	cout << "a1: Tapa" << endl;
-	cout << "a2: Tap-A-Row" << endl;
-	cout << "a3: Tap Differently" << endl;
-	cout << "a4: tatami" << endl;
+	cout << "a2: Tap-Alike" << endl;
+	cout << "a3: Tap-A-Row" << endl;
+	cout << "a4: Tap Differently" << endl;
+	cout << "a5: tatami" << endl;
 	cout << "e1: Tenner Grid" << endl;
 	cout << "e2: tents" << endl;
 	cout << "u: turnz" << endl;
@@ -22,9 +24,10 @@ int main(int argc, char **argv)
 	string str;
 	getline(cin, str);
 	if(str == "a1") solve_puz_Tapa();
-	else if(str == "a2") solve_puz_TapARow();
-	else if(str == "a3") solve_puz_TapDifferently();
-	else if(str == "a4") solve_puz_tatami();
+	else if(str == "a2") solve_puz_TapAlike();
+	else if(str == "a3") solve_puz_TapARow();
+	else if(str == "a4") solve_puz_TapDifferently();
+	else if(str == "a5") solve_puz_tatami();
 	else if(str == "e1") solve_puz_TennerGrid();
 	else if(str == "e2") solve_puz_tents();
 	else if(str == "u") solve_puz_turnz();
