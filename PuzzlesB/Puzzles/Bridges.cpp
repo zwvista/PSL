b046b881f5189975e2d97232e73a98abe6755682
zwvista <zwvista@msn.com>
2014-03-23 15:42:34 +0900

    Fixed Bridges.cpp

--- PuzzlesB/Puzzles/Bridges.cpp
+++ PuzzlesB/Puzzles/Bridges.cpp
@@ -124,8 +124,10 @@ int puz_state::find_matches(bool init)
 			nums = {0};
 			for(auto p2 = p + os; ; p2 += os){
 				char ch = cells(p2);
-				if(ch == PUZ_NUMBER)
-					nums = {0, 1, 2};
+				if(ch == PUZ_NUMBER){
+					if(m_matches.count(p2) != 0)
+						nums = {0, 1, 2};
+				}
 				else if(ch == PUZ_HORZ_1 || ch == PUZ_HORZ_2){
 					if(is_horz)
 						nums = {ch == PUZ_HORZ_1 ? 1 : 2};
