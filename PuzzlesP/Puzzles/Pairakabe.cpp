e441c733aecf835a404129413b58ef559453497f
zwvista <zwvista@msn.com>
2014-05-17 20:36:58 +0900

    Finished Pairakabe.cpp

--- PuzzlesP/Puzzles/Pairakabe.cpp
+++ PuzzlesP/Puzzles/Pairakabe.cpp
@@ -19,7 +19,7 @@ namespace puzzles{ namespace Pairakabe{
 
 #define PUZ_SPACE		' '
 #define PUZ_WALL		'W'
-#define PUZ_BOUNDARY	'B'
+#define PUZ_BOUNDARY	'+'
 
 const Position offset[] = {
 	{-1, 0},		// n
@@ -28,11 +28,28 @@ const Position offset[] = {
 	{0, -1},		// w
 };
 
+struct puz_garden
+{
+	set<Position> m_inner, m_outer;
+	int m_remaining;
+	bool m_paired = false;
+
+	puz_garden() {}
+	puz_garden(const Position& p, int cnt) : m_remaining(cnt){
+		m_inner.insert(p);
+	}
+	bool operator<(const puz_garden& x) const {
+		auto f = [](int n){ return n == 0 ? 100 : n; };
+		return f(m_outer.size()) < f(x.m_outer.size());
+	}
+};
+
 struct puz_game
 {
 	string m_id;
 	int m_sidelen;
-	map<Position, int> m_pos2garden;
+	map<Position, int> m_pos2num;
+	map<char, puz_garden> m_ch2garden;
 	string m_start;
 
 	puz_game(const ptree& attrs, const vector<string>& strs, const ptree& level);
@@ -42,25 +59,27 @@ puz_game::puz_game(const ptree& attrs, const vector<string>& strs, const ptree& 
 : m_id(attrs.get<string>("id"))
 , m_sidelen(strs.size() + 2)
 {
+	char ch_g = 'A';
 	m_start.append(m_sidelen, PUZ_BOUNDARY);
-	for(int r = 1, n = 0; r < m_sidelen - 1; ++r){
+	for(int r = 1; r < m_sidelen - 1; ++r){
 		auto& str = strs[r - 1];
 		m_start.push_back(PUZ_BOUNDARY);
 		for(int c = 1; c < m_sidelen - 1; ++c){
 			char ch = str[c - 1];
 			if(ch == PUZ_SPACE)
-				m_start.push_back(PUZ_WALL);
+				m_start.push_back(PUZ_SPACE);
 			else{
-				m_start.push_back('a' + n++);
-				m_pos2garden[{r, c}] = ch - '0';
+				int n = ch - '0';
+				Position p(r, c);
+				m_pos2num[p] = n;
+				m_ch2garden[ch_g] = {p, n - 1};
+				m_start.push_back(ch_g++);
 			}
 		}
 		m_start.push_back(PUZ_BOUNDARY);
 	}
 	m_start.append(m_sidelen, PUZ_BOUNDARY);
 }
-
-typedef pair<vector<Position>, int> puz_garden;
 
 struct puz_state : string
 {
@@ -69,18 +88,20 @@ struct puz_state : string
 	int sidelen() const {return m_game->m_sidelen;}
 	char cells(const Position& p) const { return (*this)[p.first * sidelen() + p.second]; }
 	char& cells(const Position& p) { return (*this)[p.first * sidelen() + p.second]; }
-	bool make_move(const Position& p);
+	bool make_move(char ch, const Position& p);
+	bool make_move2(char ch, const Position& p);
+	int adjust_area(bool init);
 	bool is_continuous() const;
 
 	//solve_puzzle interface
 	bool is_goal_state() const {return get_heuristic() == 0;}
 	void gen_children(list<puz_state>& children) const;
 	unsigned int get_heuristic() const {
-		return boost::accumulate(m_gardens, 0, [](int acc, const puz_garden& g){
-			return acc + g.second;
+		return boost::accumulate(m_ch2garden, 0, [](int acc, const pair<char, puz_garden>& kv){
+			return acc + kv.second.m_remaining;
 		});
 	}
-	unsigned int get_distance(const puz_state& child) const { return 1; }
+	unsigned int get_distance(const puz_state& child) const { return m_distance; }
 	void dump_move(ostream& out) const {}
 	ostream& dump(ostream& out) const;
 	friend ostream& operator<<(ostream& out, const puz_state& state) {
@@ -88,29 +109,70 @@ struct puz_state : string
 	}
 
 	const puz_game* m_game = nullptr;
-	vector<puz_garden> m_gardens;
+	map<char, puz_garden> m_ch2garden;
 	vector<set<Position>> m_2by2walls;
+	unsigned int m_distance = 0;
 };
 
 puz_state::puz_state(const puz_game& g)
 : string(g.m_start), m_game(&g)
+, m_ch2garden(g.m_ch2garden)
 {
-	for(auto& kv : g.m_pos2garden)
-		if(kv.second > 1)
-			m_gardens.push_back({{kv.first}, kv.second - 1});
-
-	boost::sort(m_gardens, [](const puz_garden& kv1, const puz_garden& kv2){
-		return kv1.second > kv2.second;
-	});
-
 	for(int r = 1; r < g.m_sidelen - 2; ++r)
 		for(int c = 1; c < g.m_sidelen - 2; ++c){
 			set<Position> rng{{r, c}, {r, c + 1}, {r + 1, c}, {r + 1, c + 1}};
 			if(boost::algorithm::all_of(rng, [&](const Position& p){
-				return cells(p) == PUZ_WALL;
+				return cells(p) == PUZ_SPACE;
 			}))
 				m_2by2walls.push_back(rng);
 		}
+
+	adjust_area(false);
+}
+
+int puz_state::adjust_area(bool init)
+{
+	for(int r = 1; r < sidelen() - 1; ++r)
+		for(int c = 1; c < sidelen() - 1; ++c){
+			Position p(r, c);
+			if(cells(p) != PUZ_SPACE) continue;
+			set<char> chars;
+			int n = 0;
+			for(auto& os : offset){
+				char ch = cells(p + os);
+				if(ch != PUZ_BOUNDARY && ch != PUZ_SPACE && ch != PUZ_WALL &&
+					chars.count(ch) == 0){
+					chars.insert(ch);
+					n += m_ch2garden.at(ch).m_paired ? 2 : 1;
+				}
+			}
+			if(n > 2)
+				cells(p) = PUZ_WALL;
+		}
+
+	for(auto& kv : m_ch2garden){
+		char ch = kv.first;
+		auto& g = kv.second;
+		auto& outer = g.m_outer;
+		outer.clear();
+		if(g.m_remaining == 0) continue;
+
+		for(auto& p : g.m_inner)
+			for(auto& os : offset){
+				auto p2 = p + os;
+				if(cells(p2) == PUZ_SPACE)
+					outer.insert(p2);
+			}
+
+		if(!init)
+			switch(outer.size()){
+			case 0:
+				return 0;
+			case 1:
+				return make_move2(ch, *outer.begin()) ? 1 : 0;
+			}
+	}
+	return 2;
 }
 
 struct puz_state2 : Position
@@ -137,59 +199,102 @@ void puz_state2::gen_children(list<puz_state2>& children) const
 bool puz_state::is_continuous() const
 {
 	set<Position> a;
-	for(int i = 0; i < length(); ++i)
-		if((*this)[i] == PUZ_WALL)
-			a.insert({i / sidelen(), i % sidelen()});
+	for(int r = 1; r < sidelen() - 1; ++r)
+		for(int c = 1; c < sidelen() - 1; ++c){
+			Position p(r, c);
+			char ch = cells(p);
+			if(ch == PUZ_SPACE || ch == PUZ_WALL)
+				a.insert(p);
+		}
 
 	list<puz_state2> smoves;
 	puz_move_generator<puz_state2>::gen_moves(a, smoves);
 	return smoves.size() == a.size();
 }
 
-bool puz_state::make_move(const Position& p)
+bool puz_state::make_move2(char ch, const Position& p)
 {
-	auto& g = m_gardens.back();
-	char ch = cells(g.first.front());
+	auto& g = m_ch2garden.at(ch);
 	cells(p) = ch;
-	g.first.push_back(p);
-	if(--g.second == 0)
-		m_gardens.pop_back();
-
+	g.m_inner.insert(p);
 	for(auto& os : offset){
 		char ch2 = cells(p + os);
-		if(ch2 != PUZ_BOUNDARY && ch2 != PUZ_WALL && ch2 != ch)
-			return false;
+		if(ch2 != PUZ_BOUNDARY && ch2 != PUZ_SPACE && ch2 != PUZ_WALL && ch2 != ch){
+			auto& g2 = m_ch2garden.at(ch2);
+			g.m_paired = true;
+			for(auto& p2 : g2.m_inner)
+				cells(p2) = ch;
+			g.m_inner.insert(g2.m_inner.begin(), g2.m_inner.end());
+			g.m_remaining += g2.m_remaining;
+			m_ch2garden.erase(ch2);
+		}
 	}
+
+	if(--g.m_remaining == 0 && g.m_paired){
+		for(auto& p2 : g.m_inner)
+			for(auto& os : offset){
+				char& ch2 = cells(p2 + os);
+				if(ch2 == PUZ_SPACE)
+					ch2 = PUZ_WALL;
+			}
+		m_ch2garden.erase(ch);
+	}
+	++m_distance;
 
 	boost::remove_erase_if(m_2by2walls, [&](const set<Position>& rng){
 		return rng.count(p) != 0;
 	});
 
+	// pruning
 	if(boost::algorithm::any_of(m_2by2walls, [&](const set<Position>& rng){
 		return boost::algorithm::none_of(rng, [&](const Position& p2){
-			return boost::algorithm::any_of(m_gardens, [&](const puz_garden& g){
-				return boost::algorithm::any_of(g.first, [&](const Position& p3){
-					return manhattan_distance(p2, p3) <= g.second;
+			return boost::algorithm::any_of(m_ch2garden, [&](const pair<char, puz_garden>& kv){
+				auto& g = kv.second;
+				return boost::algorithm::any_of(g.m_inner, [&](const Position& p3){
+					return manhattan_distance(p2, p3) <= g.m_remaining;
+				});
+			});
+		});
+	}) || boost::algorithm::any_of(m_ch2garden, [&](const pair<char, puz_garden>& kv){
+		auto& g = kv.second;
+		return !g.m_paired && boost::algorithm::none_of(kv.second.m_inner, [&](const Position& p2){
+			return boost::algorithm::any_of(m_ch2garden, [&](const pair<char, puz_garden>& kv2){
+				auto& g2 = kv2.second;
+				return kv.first != kv2.first && !g2.m_paired &&
+					boost::algorithm::any_of(g2.m_inner, [&](const Position& p3){
+					return manhattan_distance(p2, p3) <= g.m_remaining + g2.m_remaining + 1;
 				});
 			});
 		});
 	}))
 		return false;
 
-	return is_continuous();
+	adjust_area(true);
+	return !is_goal_state() || is_continuous();
+}
+
+bool puz_state::make_move(char ch, const Position& p)
+{
+	m_distance = 0;
+	if(!make_move2(ch, p))
+		return false;
+	int m;
+	while((m = adjust_area(false)) == 1);
+	return m == 2;
 }
 
 void puz_state::gen_children(list<puz_state>& children) const
 {
-	for(auto& p : m_gardens.back().first)
-		for(auto& os : offset){
-			auto p2 = p + os;
-			if(cells(p2) == PUZ_WALL){
-				children.push_back(*this);
-				if(!children.back().make_move(p2))
-					children.pop_back();
-			}
-		}
+	auto& kv = *boost::min_element(m_ch2garden, [](
+		const pair<char, puz_garden>& kv1,
+		const pair<char, puz_garden>& kv2){
+		return kv1.second < kv2.second;
+	});
+	for(auto& p : kv.second.m_outer){
+		children.push_back(*this);
+		if(!children.back().make_move(kv.first, p))
+			children.pop_back();
+	}
 }
 
 ostream& puz_state::dump(ostream& out) const
@@ -198,11 +303,11 @@ ostream& puz_state::dump(ostream& out) const
 		for(int c = 1; c < sidelen() - 1; ++c){
 			Position p(r, c);
 			char ch = cells(p);
-			if(ch == PUZ_WALL)
-				out << ch << ' ';
+			if(ch == PUZ_SPACE || ch == PUZ_WALL)
+				out << PUZ_WALL << ' ';
 			else{
-				auto it = m_game->m_pos2garden.find(p);
-				if(it == m_game->m_pos2garden.end())
+				auto it = m_game->m_pos2num.find(p);
+				if(it == m_game->m_pos2num.end())
 					out << ". ";
 				else
 					out << format("%-2d") % it->second;
--- PuzzlesP/main.cpp
+++ PuzzlesP/main.cpp
@@ -1,5 +1,6 @@
 #include "stdafx.h"
 
+extern void solve_puz_Pairakabe();
 extern void solve_puz_ParkingLot();
 extern void solve_puz_Parks();
 extern void solve_puz_Parks2();
@@ -11,22 +12,24 @@ extern void solve_puz_Pharaoh();
 
 int main(int argc, char **argv)
 {
-	cout << "a1: Parking Lot" << endl;
-	cout << "a2: Parks" << endl;
-	cout << "a3: Parks2" << endl;
-	cout << "a4: Pata" << endl;
-	cout << "a5: pathfind" << endl;
-	cout << "a6: patternpuzzle" << endl;
+	cout << "a1: Pairakabe" << endl;
+	cout << "a2: Parking Lot" << endl;
+	cout << "a3: Parks" << endl;
+	cout << "a4: Parks2" << endl;
+	cout << "a5: Pata" << endl;
+	cout << "a6: pathfind" << endl;
+	cout << "a7: patternpuzzle" << endl;
 	cout << "e: pegsolitary" << endl;
 	cout << "h: Pharaoh" << endl;
 	string str;
 	getline(cin, str);
-	if(str == "a1") solve_puz_ParkingLot();
-	else if(str == "a2") solve_puz_Parks();
-	else if(str == "a3") solve_puz_Parks2();
-	else if(str == "a4") solve_puz_Pata();
-	else if(str == "a5") solve_puz_pathfind();
-	else if(str == "a6") solve_puz_patternpuzzle();
+	if(str == "a1") solve_puz_Pairakabe();
+	else if(str == "a2") solve_puz_ParkingLot();
+	else if(str == "a3") solve_puz_Parks();
+	else if(str == "a4") solve_puz_Parks2();
+	else if(str == "a5") solve_puz_Pata();
+	else if(str == "a6") solve_puz_pathfind();
+	else if(str == "a7") solve_puz_patternpuzzle();
 	else if(str == "e") solve_puz_pegsolitary();
 	else if(str == "h") solve_puz_Pharaoh();
 
