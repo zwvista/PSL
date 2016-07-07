9812e043665205c39fc07a5534d3b785fa7a8f71
zhaowei <zwvista@msn.com>
2016-07-07 18:54:36 +0900

    Added AbstractPainting.cpp

--- PuzzlesA/main.cpp
+++ PuzzlesA/main.cpp
@@ -1,18 +1,21 @@
 #include "stdafx.h"
 
 extern void solve_puz_Abc();
+extern void solve_puz_AbstractPainting();
 extern void solve_puz_Archipelago();
 extern void solve_puz_Arrows();
 
 int main(int argc, char **argv)
 {
-    cout << "b: Abc" << endl;
+    cout << "b1: Abc" << endl;
+    cout << "b2: Abstract Painting" << endl;
     cout << "r1: Archipelago" << endl;
     cout << "r2: Arrows" << endl;
     string str;
     getline(cin, str);
     if(str.empty());
     else if(str == "b") solve_puz_Abc();
+    else if(str == "b") solve_puz_AbstractPainting();
     else if(str == "r1") solve_puz_Archipelago();
     else if(str == "r2") solve_puz_Arrows();
 
