fa4588c572401619d8aecbf5b94337a975108641
zhaowei <zwvista@msn.com>
2016-07-11 16:24:45 +0900

    Modified AbstractPainting.cpp

--- PuzzlesA/Puzzles/AbstractPainting.cpp
+++ PuzzlesA/Puzzles/AbstractPainting.cpp
@@ -17,7 +17,7 @@
 
 namespace puzzles{ namespace AbstractPainting{
 
-#define PUZ_TREE        'T'
+#define PUZ_PAINTING    'P'
 #define PUZ_SPACE       '.'
 #define PUZ_UNKNOWN     -1
 
@@ -35,14 +35,21 @@ const Position offset2[] = {
     {0, 0},         // w
 };
 
+struct puz_region
+{
+    vector<Position> m_rng;
+    map<int, int> m_rc2count;
+};
+
 struct puz_game    
 {
     string m_id;
     int m_sidelen;
-    vector<int> m_painting_counts_rows, m_painting_counts_cols;
+    vector<int> m_painting_counts;
     int m_painting_total_count;
-    vector<vector<Position>> m_regions;
+    vector<puz_region> m_regions;
     map<Position, int> m_pos2region;
+    map<pair<int, int>, int> m_rc_region2count;
     set<Position> m_horz_walls, m_vert_walls;
 
     puz_game(const ptree& attrs, const vector<string>& strs, const ptree& level);
@@ -75,8 +82,7 @@ void puz_state2::gen_children(list<puz_state2>& children) const
 puz_game::puz_game(const ptree& attrs, const vector<string>& strs, const ptree& level)
     : m_id(attrs.get<string>("id"))
     , m_sidelen(strs.size() / 2 - 1)
-    , m_painting_counts_rows(m_sidelen)
-    , m_painting_counts_cols(m_sidelen)
+    , m_painting_counts(m_sidelen * 2)
 {
     set<Position> rng;
     for(int r = 0;; ++r){
@@ -99,119 +105,45 @@ puz_game::puz_game(const ptree& attrs, const vector<string>& strs, const ptree& 
 
     auto f = [](const string& s) { return s == "  " ? PUZ_UNKNOWN : stoi(s); };
     for(int i = 0; i < m_sidelen; ++i){
-        m_painting_counts_rows[i] = f(strs[i * 2 + 1].substr(m_sidelen * 2 + 1, 2));
-        m_painting_counts_cols[i] = f(strs[m_sidelen * 2 + 1].substr(i * 2, 2));
+        m_painting_counts[i] = f(strs[i * 2 + 1].substr(m_sidelen * 2 + 1, 2));
+        m_painting_counts[i + m_sidelen] = f(strs[m_sidelen * 2 + 1].substr(i * 2, 2));
     }
+    m_painting_total_count = boost::accumulate(m_painting_counts, 0) / 2;
 
     for(int n = 0; !rng.empty(); ++n){
         list<puz_state2> smoves;
         puz_move_generator<puz_state2>::gen_moves({m_horz_walls, m_vert_walls, *rng.begin()}, smoves);
-        vector<Position> rng2;
+        puz_region region;
         for(auto& p : smoves){
             m_pos2region[p] = n;
             rng.erase(p);
-            rng2.push_back(p);
+            region.m_rng.push_back(p);
+            ++region.m_rc2count[p.first];
+            ++region.m_rc2count[p.second + m_sidelen];
         }
-        m_regions.push_back(rng2);
+        m_regions.push_back(region);
     }
 }
-
-// first : all the remaining positions in the area where a tree can be planted
-// second : the number of trees that need to be planted in the area
-struct puz_area : pair<vector<Position>, int>
-{
-    puz_area() {}
-    puz_area(int tree_count_area)
-        : pair<vector<Position>, int>({}, tree_count_area)
-    {}
-    void add_cell(const Position& p){ first.push_back(p); }
-    void remove_cell(const Position& p){ boost::remove_erase(first, p); }
-    void plant_tree(const Position& p){ remove_cell(p); --second; }
-    bool is_valid() const {
-        // if second < 0, that means too many trees have been planted in this area
-        // if first.size() < second, that means there are not enough positions
-        // for the trees to be planted
-        return second >= 0 && first.size() >= second;
-    }
-};
-
-// all of the areas in the group
-struct puz_group : vector<puz_area>
-{
-    puz_group() {}
-    puz_group(int cell_count, int tree_count_area)
-        : vector<puz_area>(cell_count, puz_area(tree_count_area)) {}
-    bool is_valid() const {
-        return boost::algorithm::all_of(*this, [](const puz_area& a) {
-            return a.is_valid();
-        });
-    }
-};
-
-struct puz_groups
-{
-    puz_groups() {}
-    puz_groups(const puz_game* g)
-        : m_game(g)
-        , m_parks(g->m_sidelen, g->m_painting_total_count)
-        , m_rows(g->m_sidelen, g->m_painting_total_count)
-        , m_cols(g->m_sidelen, g->m_painting_total_count)
-    {}
-
-    array<puz_area*, 3> get_areas(const Position& p){
-        return {
-            &m_parks[m_game->m_pos2region.at(p)],
-            &m_rows[p.first],
-            &m_cols[p.second]
-        };
-    }
-    void add_cell(const Position& p){
-        for(auto a : get_areas(p))
-            a->add_cell(p);
-    }
-    void remove_cell(const Position& p){
-        for(auto a : get_areas(p))
-            a->remove_cell(p);
-    }
-    void plant_tree(const Position& p){
-        for(auto a : get_areas(p)){
-            a->plant_tree(p);
-            if(a->second == 0){
-                // copy the range
-                auto rng = a->first;
-                for(auto& p2 : rng)
-                    remove_cell(p2);
-            }
-        }
-    }
-    bool is_valid() const {
-        return m_parks.is_valid() && m_rows.is_valid() && m_cols.is_valid();
-    }
-    const puz_area& get_best_candidate_area() const;
-
-    const puz_game* m_game;
-    puz_group m_parks;
-    puz_group m_rows;
-    puz_group m_cols;
-};
 
 struct puz_state : string
 {
     puz_state() {}
     puz_state(const puz_game& g);
-    int sidelen() const { return m_groups.m_game->m_sidelen; }
+    int sidelen() const { return m_game->m_sidelen; }
     bool is_valid(const Position& p) const {
         return p.first >= 0 && p.first < sidelen() && p.second >= 0 && p.second < sidelen();
     }
     char cells(const Position& p) const { return (*this)[p.first * sidelen() + p.second]; }
     char& cells(const Position& p) { return (*this)[p.first * sidelen() + p.second]; }
-    bool make_move(const Position& p);
+    bool make_move(int n);
+    bool make_move2(int n);
+    int find_matches(bool init);
 
     // solve_puzzle interface
     bool is_goal_state() const {return get_heuristic() == 0;}
     void gen_children(list<puz_state>& children) const;
     unsigned int get_heuristic() const {
-        return m_groups.m_game->m_painting_total_count - boost::count(*this, PUZ_TREE);
+        return m_game->m_painting_total_count - boost::count(*this, PUZ_PAINTING);
     }
     unsigned int get_distance(const puz_state& child) const {return 1;}
     void dump_move(ostream& out) const {}
@@ -220,53 +152,71 @@ struct puz_state : string
         return state.dump(out);
     }
 
-    puz_groups m_groups;
+    const puz_game* m_game = nullptr;
+    vector<int> m_painting_counts;
+    // key: index of the row/column
+    // value.elem: index of the region that can be revealed
+    map<int, vector<int>> m_matches;
+    unsigned int m_distance = 0;
 };
 
 puz_state::puz_state(const puz_game& g)
-: string(g.m_sidelen * g.m_sidelen, PUZ_SPACE)
-, m_groups(&g)
+    : string(g.m_sidelen * g.m_sidelen, PUZ_SPACE)
+    , m_game(&g)
+    , m_painting_counts(g.m_painting_counts)
 {
-    for(int r = 0; r < g.m_sidelen; ++r)
-        for(int c = 0; c < g.m_sidelen; ++c)
-            m_groups.add_cell({r, c});
+    for(int i = 0; i < g.m_regions.size(); ++i)
+        for(auto& kv : g.m_regions[i].m_rc2count)
+            m_matches[kv.first].push_back(i);
 
+    find_matches(true);
 }
 
-const puz_area& puz_groups::get_best_candidate_area() const
+int puz_state::find_matches(bool init)
 {
-    vector<const puz_area*> areas;
-    for(auto grp : {&m_parks, &m_rows, &m_cols})
-        for(auto& a : *grp)
-            if(a.second > 0)
-                areas.push_back(&a);
+    for(auto& kv : m_matches){
+        int rc = kv.first;
+        auto& region_ids = kv.second;
 
-    return **boost::min_element(areas, [](const puz_area* a1, const puz_area* a2){
-        return a1->first.size() < a2->first.size();
-    });
-}
+        boost::remove_erase_if(region_ids, [&](int id){
+            return m_game->m_regions[id].m_rc2count.at(rc) > m_painting_counts[rc];
+        });
 
-bool puz_state::make_move(const Position& p)
-{
-    cells(p) = PUZ_TREE;
-    m_groups.plant_tree(p);
-
-    // no touch
-    for(auto& os : offset){
-        auto p2 = p + os;
-        if(is_valid(p2))
-            m_groups.remove_cell(p2);
+        if(!init)
+            switch(region_ids.size()){
+            case 0:
+                return 0;
+            case 1:
+                return make_move2(*region_ids.begin()) ? 1 : 0;
+            }
     }
+    return 2;
+}
 
-    return m_groups.is_valid();
+bool puz_state::make_move2(int n)
+{
+    auto& region = m_game->m_regions[n];
+    for(auto& p : region.m_rng)
+        cells(p) = PUZ_PAINTING;
+}
+
+bool puz_state::make_move(int n)
+{
+    auto& region = m_game->m_regions[n];
+    for(auto& p : region.m_rng)
+        cells(p) = PUZ_PAINTING;
 }
 
 void puz_state::gen_children(list<puz_state>& children) const
 {
-    auto& a = m_groups.get_best_candidate_area();
-    for(auto& p : a.first){
+    auto& kv = *boost::min_element(m_matches, [](
+        const pair<const int, vector<int>>& kv1,
+        const pair<const int, vector<int>>& kv2){
+        return kv1.second.size() < kv2.second.size();
+    });
+    for(int n : kv.second){
         children.push_back(*this);
-        if(!children.back().make_move(p))
+        if(!children.back().make_move(n))
             children.pop_back();
     }
 }
@@ -276,13 +226,13 @@ ostream& puz_state::dump(ostream& out) const
     for(int r = 0;; ++r){
         // draw horz-walls
         for(int c = 0; c < sidelen(); ++c)
-            out << (m_groups.m_game->m_horz_walls.count({r, c}) == 1 ? " -" : "  ");
+            out << (m_game->m_horz_walls.count({r, c}) == 1 ? " -" : "  ");
         out << endl;
         if(r == sidelen()) break;
         for(int c = 0;; ++c){
             Position p(r, c);
             // draw vert-walls
-            out << (m_groups.m_game->m_vert_walls.count(p) == 1 ? '|' : ' ');
+            out << (m_game->m_vert_walls.count(p) == 1 ? '|' : ' ');
             if(c == sidelen()) break;
             out << cells(p);
         }
