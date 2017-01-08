cbee6362b9451ba4b2d8bdc4b6de404d2e540e06
zwvista <zwvista@msn.com>
2017-01-08 23:52:59 +0900

    hopover fixed

--- PuzzlesH/Puzzles/hopover.cpp
+++ PuzzlesH/Puzzles/hopover.cpp
@@ -41,14 +41,14 @@ ostream & operator<<(ostream &out, const puz_step &mi)
     return out;
 }
 
-struct puz_state : string
+struct puz_state : pair<string, boost::optional<puz_step>>
 {
     puz_state() {}
     puz_state(const puz_game& g)
-        : string(g.m_start), m_game(&g) {}
+        : pair<string, boost::optional<puz_step>>{g.m_start, {}}, m_game(&g) {}
     void make_move(int i, int j){
-        ::swap((*this)[i], (*this)[j]);
-        m_move = puz_step(i, j);
+        ::swap(first[i], first[j]);
+        second = puz_step(i, j);
     }
 
     // solve_puzzle interface
@@ -56,38 +56,37 @@ struct puz_state : string
     void gen_children(list<puz_state>& children) const;
     unsigned int get_heuristic() const {
         int n = 0;
-        for(size_t i = 1; i < length() - 1; ++i)
-            n += myabs(at(i) - m_game->m_goal.at(i));
+        for(size_t i = 1; i < first.length() - 1; ++i)
+            n += myabs(first[i] - m_game->m_goal[i]);
         return n;
     }
     unsigned int get_distance(const puz_state& child) const {return 1;}
-    void dump_move(ostream& out) const {if(m_move) out << *m_move;}
+    void dump_move(ostream& out) const {if(second) out << *second;}
     ostream& dump(ostream& out) const;
     friend ostream& operator<<(ostream& out, const puz_state& state) {
         return state.dump(out);
     }
 
     const puz_game* m_game;
-    boost::optional<puz_step> m_move;
 };
 
 void puz_state::gen_children(list<puz_state>& children) const
 {
-    for(size_t i = 1; i < length() - 1; ++i){
-        char n = at(i);
+    for(size_t i = 1; i < first.length() - 1; ++i){
+        char n = first[i];
         if(n == PUZ_RED || n == PUZ_BLUE){
             bool can_move = false;
             // Red frogs move rightward and blue ones, leftward.
             int delta = n == PUZ_RED ? 1 : -1;
             int j = i + delta;
-            char n2 = at(j);
+            char n2 = first[j];
             // A frog can move forward
             if(n2 == PUZ_SPACE)
                 can_move = true;
             // or jump a frog in front of it
             else if(n2 == PUZ_RED || n2 == PUZ_BLUE){
                 j += delta;
-                n2 = at(j);
+                n2 = first[j];
                 if(n2 == PUZ_SPACE)
                     can_move = true;
             }
@@ -102,8 +101,8 @@ void puz_state::gen_children(list<puz_state>& children) const
 ostream& puz_state::dump(ostream& out) const
 {
     dump_move(out);
-    for(size_t j = 1; j < size() - 1; ++j)
-        out << at(j) << " ";
+    for(size_t j = 1; j < first.size() - 1; ++j)
+        out << first[j] << " ";
     out << endl;
     return out;
 }
