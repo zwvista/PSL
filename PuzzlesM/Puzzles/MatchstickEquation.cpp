c59e8e2c05205d40e7fc54c1e980ecb34811933b
zwvista <zwvista@msn.com>
2017-04-12 07:53:11 +0900

    MatchstickEquation 2

--- PuzzlesM/Puzzles/MatchstickEquation.cpp
+++ PuzzlesM/Puzzles/MatchstickEquation.cpp
@@ -12,56 +12,111 @@ namespace puzzles{ namespace MatchstickEquation{
 #define PUZ_ADD           1
 #define PUZ_MOVE          2
 
-typedef pair<Position, Position> puz_matchstick;
+//  . - .1
+// 2|   |4
+//  . - .8
+//16|   |32
+//  . - .64
+const int matchstick_digits[] = {
+    0b1110111, // 0
+    0b0100100, // 1
+    0b1011101, // 2
+    0b1101101, // 3
+    0b0101110, // 4
+    0b1101011, // 5
+    0b1111011, // 6
+    0b0100101, // 7
+    0b1111111, // 8
+    0b1101111, // 9
+};
+
+// operator from, operator to, matchstick to remove, matchstick to add
+const map<char, tuple<char, int, int>> op2op = {
+    {'+', {'-', 1, 0}},
+    {'-', {'+', 0, 1}},
+    {'*', {'/', 1, 0}},
+    {'/', {'*', 0, 1}},
+};
 
 struct puz_game
 {
     string m_id;
-    Position m_size;
-    set<Position> m_dots;
-    set<puz_matchstick> m_matchsticks;
-    int m_action, m_move_count, m_square_count;
-    set<puz_matchstick> m_possible_matchsticks;
+    int m_remove_count, m_add_count;
+    // digit from * 10 + digit to, matchstick to remove, matchstick to add
+    vector<pair<int, int>> m_digit2digit;
+    vector<int> m_digits, m_indexes;
+    char m_operator;
 
     puz_game(const vector<string>& strs, const xml_node& level);
-    int rows() const { return m_size.first; }
-    int cols() const { return m_size.second; }
-    bool is_valid_dot(const Position& p) const { return m_dots.count(p) != 0; }
 };
 
 puz_game::puz_game(const vector<string>& strs, const xml_node& level)
     : m_id(level.attribute("id").value())
-    , m_move_count(level.attribute("matchsticks").as_int())
+    , m_digit2digit(100)
+    , m_indexes{0}
 {
+    int n = level.attribute("matchsticks").as_int();
     string action = level.attribute("action").value();
-    m_action = action == "add" ? PUZ_ADD : action == "move" ? PUZ_MOVE : PUZ_REMOVE;
+    if(action == "remove")
+        m_remove_count = n, m_add_count = 0;
+    else if(action == "add")
+        m_remove_count = 0, m_add_count = n;
+    else
+        m_remove_count = m_add_count = n;
+    for(int i = 0; i < 10; ++i){
+        int d1 = matchstick_digits[i];
+        for(int j = 0; j < 10; ++j){
+            int d2 = matchstick_digits[j];
+            auto& v = m_digit2digit[i * 10 + j];
+            int n1 = 0, n2 = 0;
+            for(int k = 0; k < 7; ++k){
+                int b1 = d1 & 1 << k;
+                int b2 = d2 & 1 << k;
+                if(b1 && !b2) ++n1;
+                else if(!b1 && b2) ++n2;
+            }
+            v = make_pair(n1, n2);
+        }
+    }
+    string equation = level.attribute("equation").value();
+    int operand1, operand2, result;
+    qi::phrase_parse(equation.begin(), equation.end(),
+        qi::int_[phx::ref(operand1) = qi::_1] >>
+        qi::char_[phx::ref(m_operator) = qi::_1] >>
+        qi::int_[phx::ref(operand2) = qi::_1] >> '=' >>
+        qi::int_[phx::ref(result) = qi::_1], qi::space);
+    auto f = [&](int n){
+        int index = m_indexes.back();
+        do{
+            m_digits.push_back(n % 10);
+            ++index;
+            n /= 10;
+        }while(n != 0);
+        m_indexes.push_back(index);
+    };
+    f(operand1);
+    f(operand2);
+    f(result);
 }
 
 struct puz_state
 {
     puz_state() {}
     puz_state(const puz_game& g);
-    int rows() const { return m_game->rows(); }
-    int cols() const { return m_game->cols(); }
-    bool operator<(const puz_state& x) const { return m_matchsticks < x.m_matchsticks; }
-    bool is_valid_dot(const Position& p) const { return m_game->is_valid_dot(p); }
-    bool is_matchstick(const puz_matchstick& m) const { return m_matchsticks.count(m) != 0; }
-    void check_squares();
-    void make_move(function<void()> f);
-    void make_move_remove(const puz_matchstick& m) { make_move([&]{m_matchsticks.erase(m);}); }
-    void make_move_add(const puz_matchstick& m) { make_move([&]{m_matchsticks.insert(m);}); }
-    void make_move_move(const puz_matchstick& m1, const puz_matchstick& m2) {
-        make_move([&]{m_matchsticks.erase(m1); m_matchsticks.insert(m2);});
+    bool operator<(const puz_state& x) const { 
+        return tie(m_digits, m_operator, m_index) < tie(x.m_digits, x.m_operator, x.m_index);
     }
+    void prepare_equation(int& operand1, int& operand2, int& result) const;
+    bool is_valid_equation() const;
+    void make_move(int digit_to);
+    void make_move(char op_to);
 
     //solve_puzzle interface
     bool is_goal_state() const {
-        return m_is_valid_state && get_heuristic() == 0;
+        return get_heuristic() == 0 && is_valid_equation();
     }
     void gen_children(list<puz_state>& children) const;
-    unsigned int get_heuristic() const {
-        return m_move_count * 10 + abs(m_square_count - m_game->m_square_count);
-    }
+    unsigned int get_heuristic() const { return m_remove_count + m_add_count; }
     unsigned int get_distance(const puz_state& child) const { return m_distance; }
     void dump_move(ostream& out) const {}
     ostream& dump(ostream& out) const;
@@ -70,110 +125,101 @@ struct puz_state
     }
 
     const puz_game* m_game = nullptr;
-    set<puz_matchstick> m_matchsticks;
-    int m_move_count;
-    int m_square_count = 0;
-    bool m_is_valid_state = false;
+    int m_remove_count, m_add_count;
+    vector<int> m_digits;
+    char m_operator;
+    int m_index = 0;
     unsigned int m_distance = 0;
 };
 
 puz_state::puz_state(const puz_game& g)
-: m_game(&g), m_matchsticks(g.m_matchsticks), m_move_count(g.m_move_count)
+: m_game(&g), m_remove_count(g.m_remove_count), m_add_count(g.m_add_count)
+, m_digits(g.m_digits), m_operator(g.m_operator)
 {
-    check_squares();
 }
 
-void puz_state::check_squares()
+void puz_state::prepare_equation(int& operand1, int& operand2, int& result) const
 {
-    m_square_count = 0;
-    set<puz_matchstick> matchsticks_in_square;
-    for(int r = 0; r < rows() - 1; ++r)
-        for(int c = 0; c < cols() - 1; ++c){
-            for(int n = 1;; ++n){
-                for(int i = 0; i <= n; ++i){
-                    if(!is_valid_dot({r, c + i})) goto next_dot;
-                    if(!is_valid_dot({r + n, c + i})) goto next_dot;
-                    if(!is_valid_dot({r + i, c})) goto next_dot;
-                    if(!is_valid_dot({r + i, c + n})) goto next_dot;
-                }
-                vector<puz_matchstick> matchsticks;
-                auto f = [&](int r1, int c1, int r2, int c2) {
-                    puz_matchstick m({r1, c1}, {r2, c2});
-                    if(!is_matchstick(m)) return false;
-                    matchsticks.push_back(m);
-                    return true;
-                };
-                for(int i = 0; i < n; ++i){
-                    if(!f(r, c + i, r, c + i + 1)) goto next_square;
-                    if(!f(r + n, c + i, r + n, c + i + 1)) goto next_square;
-                    if(!f(r + i, c, r + i + 1, c)) goto next_square;
-                    if(!f(r + i, c + n, r + i + 1, c + n)) goto next_square;
-                }
-                matchsticks_in_square.insert(matchsticks.begin(), matchsticks.end());
-                ++m_square_count;
-            next_square:;
-            }
-        next_dot:;
-        }
-    m_is_valid_state = m_matchsticks == matchsticks_in_square;
+    auto f = [&](int& n, int idx){
+        auto& indexes = m_game->m_indexes;
+        n = 0;
+        for(int i = indexes[idx + 1] - 1; i >= indexes[idx]; --i)
+            n = n * 10 + m_digits[i];
+    };
+    f(operand1, 0);
+    f(operand2, 1);
+    f(result, 2);
 }
 
-void puz_state::make_move(function<void()> f)
+bool puz_state::is_valid_equation() const
 {
-    int d = get_heuristic();
-    f();
-    check_squares();
-    --m_move_count;
-    m_distance = get_heuristic() - d;
+    int operand1, operand2, result;
+    prepare_equation(operand1, operand2, result);
+    switch(m_operator){
+    case '+':
+        return operand1 + operand2 == result;
+    case '-':
+        return operand1 - operand2 == result;
+    case '*':
+        return operand1 * operand2 == result;
+    case '/':
+        return operand1 / operand2 == result && operand1 / operand2 * operand2 == operand1;
+    default:
+        return false;
+    }
+}
+
+void puz_state::make_move(int digit_to)
+{
+    auto d = get_heuristic();
+    int& digit_from = m_digits[m_index++];
+    auto& kv = m_game->m_digit2digit[digit_from * 10 + digit_to];
+    m_remove_count -= kv.first, m_add_count -= kv.second;
+    digit_from = digit_to;
+    m_distance = d - get_heuristic();
+}
+
+void puz_state::make_move(char op_to)
+{
+    auto d = get_heuristic();
+    ++m_index;
+    if(m_operator != op_to){
+        auto& t = op2op.at(m_operator);
+        m_remove_count -= get<1>(t), m_add_count -= get<2>(t);
+        m_operator = op_to;
+    }
+    m_distance = d - get_heuristic();
 }
 
 void puz_state::gen_children(list<puz_state>& children) const
 {
-    if(m_move_count == 0) return;
-    set<puz_matchstick> matchsticks;
-    switch(m_game->m_action){
-    case PUZ_REMOVE:
-        for(auto& m : m_matchsticks){
+    int sz = m_digits.size();
+    if(m_index == sz){
+        children.push_back(*this);
+        children.back().make_move(m_operator);
+        auto& t = op2op.at(m_operator);
+        if(get<1>(t) <= m_remove_count && get<2>(t) <= m_add_count){
             children.push_back(*this);
-            children.back().make_move_remove(m);
+            children.back().make_move(get<0>(t));
         }
-        break;
-    case PUZ_ADD:
-        boost::set_difference(m_game->m_possible_matchsticks, m_matchsticks, inserter(matchsticks, matchsticks.end()));
-        for(auto& m : matchsticks){
-            children.push_back(*this);
-            children.back().make_move_add(m);
-        }
-        break;
-    case PUZ_MOVE:
-        boost::set_difference(m_game->m_possible_matchsticks, m_matchsticks, inserter(matchsticks, matchsticks.end()));
-        for(auto& m1 : m_matchsticks)
-            for(auto& m2 : matchsticks){
+    }
+    else if(m_index < sz){
+        int digit_from = m_digits[m_index];
+        for(int digit_to = 0; digit_to < 10; ++digit_to){
+            auto& kv = m_game->m_digit2digit[digit_from * 10 + digit_to];
+            if(kv.first <= m_remove_count && kv.second <= m_add_count){
                 children.push_back(*this);
-                children.back().make_move_move(m1, m2);
+                children.back().make_move(digit_to);
             }
-        break;
+        }
     }
 }
 
 ostream& puz_state::dump(ostream& out) const
 {
-    for(int r = 0;; ++r){
-        for(int c = 0;; ++c){
-            Position p(r, c);
-            out << (is_valid_dot(p) ? '.' : ' ');
-            if(c == cols() - 1) break;
-            out << (is_matchstick({p, Position(r, c + 1)}) ? '-' : ' ');
-        }
-        out << endl;
-        if(r == rows() - 1) break;
-        for(int c = 0;; ++c){
-            out << (is_matchstick({Position(r, c), Position(r + 1, c)}) ? '|' : ' ');
-            if(c == cols() - 1) break;
-            out << ' ';
-        }
-        out << endl;
-    }
+    int operand1, operand2, result;
+    prepare_equation(operand1, operand2, result);
+    out << format("%1%%2%%3%=%4%\n") % operand1 % m_operator % operand2 % result;
     return out;
 }
 
