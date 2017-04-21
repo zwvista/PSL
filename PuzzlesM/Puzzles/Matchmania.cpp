5d9bcc8b183de357d982206abc293170ed3be82d
zwvista <zwvista@msn.com>
2017-04-21 21:29:46 +0900

    Matchmania 3

--- PuzzlesM/Puzzles/Matchmania.cpp
+++ PuzzlesM/Puzzles/Matchmania.cpp
@@ -22,8 +22,10 @@ const Position offset[] = {
 
 struct puz_bunny_info
 {
+    char m_bunny_name, m_food_name;
 	Position m_origin;
-	vector<Position> m_food;
+	set<Position> m_food;
+    int steps() const { return m_food.size() + 1; }
 };
 
 struct puz_game
@@ -32,7 +34,7 @@ struct puz_game
     Position m_size;
     string m_start;
     map<char, puz_bunny_info> m_name2info;
-    vector<Position> m_holes;
+    set<Position> m_holes, m_food2;
 
     puz_game(const vector<string>& strs, const xml_node& level);
     int rows() const { return m_size.first; }
@@ -53,13 +55,24 @@ puz_game::puz_game(const vector<string>& strs, const xml_node& level)
         	m_start.push_back(ch);
         	switch(ch){
             case 'C':
-            	m_name2info[ch].m_origin = p;
-            	break;
+            {
+                auto& info = m_name2info[ch];
+                info.m_bunny_name = ch;
+                info.m_origin = p;
+                break;
+            }
             case 'c':
-            	m_name2info[toupper(ch)].m_food.push_back(p);
+            {
+                auto& info = m_name2info[toupper(ch)];
+                info.m_food_name = ch;
+                info.m_food.insert(p);
+                break;
+            }
+            case 'm':
+                m_food2.insert(p);
         		break;
             case PUZ_HOLE:
-            	m_holes.push_back(p);
+            	m_holes.insert(p);
         		break;
             }
         }
@@ -94,7 +107,11 @@ struct puz_state
         return get_heuristic() == 0;
     }
     void gen_children(list<puz_state>& children) const;
-    unsigned int get_heuristic() const { return 1; }
+    unsigned int get_heuristic() const { 
+        return boost::accumulate(m_name2info, 0, [](int acc, const pair<const char, puz_bunny_info>& kv){
+            return acc + kv.second.steps();
+        }) + m_food2.size();
+    }
     unsigned int get_distance(const puz_state& child) const { return m_distance; }
     void dump_move(ostream& out) const { if(m_move) out << *m_move; }
     ostream& dump(ostream& out) const;
@@ -104,12 +121,41 @@ struct puz_state
 
     const puz_game* m_game = nullptr;
     string m_cells;
+    map<char, puz_bunny_info> m_name2info;
+    set<Position> m_food2;
     unsigned int m_distance = 0;
     boost::optional<puz_step> m_move;
 };
 
+struct puz_state2 : Position
+{
+    puz_state2(const puz_state& s, const puz_bunny_info& info)
+        : m_state(&s), m_info(&info) { make_move(info.m_origin); }
+
+    void make_move(const Position& p){ static_cast<Position&>(*this) = p; }
+    void gen_children(list<puz_state2>& children) const;
+
+    const puz_state* m_state;
+    const puz_bunny_info* m_info;
+};
+
+void puz_state2::gen_children(list<puz_state2>& children) const
+{
+    char ch = m_state->cells(*this);
+    if(ch != m_info->m_bunny_name && ch != PUZ_SPACE) return;
+    for(auto& os : offset){
+        auto p = *this + os;
+        ch = m_state->cells(p);
+        if(ch == m_info->m_bunny_name || ch == PUZ_SPACE){
+            children.push_back(*this);
+            children.back().make_move(p);
+        }
+    }
+}
+
 puz_state::puz_state(const puz_game& g)
 : m_game(&g), m_cells(g.m_start)
+, m_name2info(g.m_name2info), m_food2(g.m_food2)
 {
 }
 
