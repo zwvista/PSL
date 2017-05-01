506e041b3e6293de57cef9101d4ab70de8a2bb84
趙偉 <zwvista@msn.com>
2017-05-01 16:11:14 +0900

    MazeEditor3

---
+++ MazeEditor3/MazeEditor3/AppDelegate.swift
@@ -0,0 +1,26 @@
+//
+//  AppDelegate.swift
+//  MazeEditor3
+//
+//  Created by 趙偉 on 2017/05/01.
+//  Copyright © 2017年 趙偉. All rights reserved.
+//
+
+import Cocoa
+
+@NSApplicationMain
+class AppDelegate: NSObject, NSApplicationDelegate {
+
+
+
+    func applicationDidFinishLaunching(_ aNotification: Notification) {
+        // Insert code here to initialize your application
+    }
+
+    func applicationWillTerminate(_ aNotification: Notification) {
+        // Insert code here to tear down your application
+    }
+
+
+}
+
