3d4dbab7b9ccd7c947944874dab0ed79ad8e8b5e
趙偉 <zwvista@msn.com>
2019-09-07 01:27:25 +0900

    ZenGardens 1

--- PuzzlesZ/main.cpp
+++ PuzzlesZ/main.cpp
@@ -1,6 +1,7 @@
 #include "stdafx.h"
 
 extern void solve_puz_zafiro();
+extern void solve_puz_ZenGardens();
 extern void solve_puz_ZenLandscaper();
 extern void solve_puz_zgj();
 
@@ -8,13 +9,15 @@ int main(int argc, char **argv)
 {
     srand(time(0));
     cout << "a: zafiro" << endl;
-    cout << "e: Zen Landscaper" << endl;
+    cout << "e1: Zen Gardens" << endl;
+    cout << "e2: Zen Landscaper" << endl;
     cout << "g: zgj" << endl;
     string str;
     getline(cin, str);
     if (str.empty());
     else if (str == "a") solve_puz_zafiro();
-    else if (str == "e") solve_puz_ZenLandscaper();
+    else if (str == "e1") solve_puz_ZenGardens();
+    else if (str == "e2") solve_puz_ZenLandscaper();
     else if (str == "g") solve_puz_zgj();
 
     return 0;
