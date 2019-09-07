403403c9dd4f2da7901a0941696dd1c1f35edba2
趙偉 <zwvista@msn.com>
2019-09-07 11:59:59 +0900

    Warehouse 1

--- PuzzlesW/main.cpp
+++ PuzzlesW/main.cpp
@@ -3,6 +3,7 @@
 extern void solve_puz_Walls();
 extern void solve_puz_WallHints();
 extern void solve_puz_WallSentinels();
+extern void solve_puz_Warehouse();
 extern void solve_puz_WishSandwich();
 extern void solve_puz_Wriggle();
 
@@ -12,6 +13,7 @@ int main(int argc, char **argv)
     cout << "a1: Walls" << endl;
     cout << "a2: Wall Hints" << endl;
     cout << "a3: Wall Sentinels" << endl;
+    cout << "a4: Warehouse" << endl;
     cout << "i: Wish Sandwich" << endl;
     cout << "r: Wriggle" << endl;
     string str;
@@ -20,6 +22,7 @@ int main(int argc, char **argv)
     else if (str == "a1") solve_puz_Walls();
     else if (str == "a2") solve_puz_WallHints();
     else if (str == "a3") solve_puz_WallSentinels();
+    else if (str == "a4") solve_puz_Warehouse();
     else if (str == "i") solve_puz_WishSandwich();
     else if (str == "r") solve_puz_Wriggle();
 
