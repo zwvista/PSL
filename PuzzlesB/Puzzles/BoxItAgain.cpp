3a508e40e325e2f8ec213f26b98e4daeeea28d8c
趙偉 <zwvista@msn.com>
2019-09-12 01:08:14 +0900

    cells

--- PuzzlesB/Puzzles/BoxItAgain.cpp
+++ PuzzlesB/Puzzles/BoxItAgain.cpp
@@ -88,13 +88,16 @@ puz_game::puz_game(const vector<string>& strs, const xml_node& level)
     }
 }
 
-struct puz_state : string
+struct puz_state
 {
     puz_state() {}
     puz_state(const puz_game& g);
     int sidelen() const {return m_game->m_sidelen;}
-    char cells(int r, int c) const { return (*this)[r * sidelen() + c]; }
-    char& cells(int r, int c) { return (*this)[r * sidelen() + c]; }
+    char cells(int r, int c) const { return m_cells[r * sidelen() + c]; }
+    char& cells(int r, int c) { return m_cells[r * sidelen() + c]; }
+    bool operator<(const puz_state& x) const {
+        return tie(m_cells, m_matches) < tie(x.m_cells, x.m_matches);
+    }
     bool make_move(const Position& p, int n);
     void make_move2(const Position& p, int n);
     int find_matches(bool init);
@@ -111,6 +114,7 @@ struct puz_state : string
     }
 
     const puz_game* m_game = nullptr;
+    string m_cells;
     // key: the position of the number
     // value.elem: the index of the box
     map<Position, vector<int>> m_matches;
@@ -120,7 +124,7 @@ struct puz_state : string
 };
 
 puz_state::puz_state(const puz_game& g)
-: string(g.m_sidelen * g.m_sidelen, PUZ_SPACE), m_game(&g)
+: m_game(&g), m_cells(g.m_sidelen * g.m_sidelen, PUZ_SPACE)
 {
     for (auto& kv : g.m_pos2boxinfo) {
         auto& box_ids = m_matches[kv.first];
