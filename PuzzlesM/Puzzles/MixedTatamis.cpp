32be8d3dcb808d5f9015c30f5a4faa9bc319f1f6
zwvista <zwvista@msn.com>
2019-09-12 21:42:49 +0900

    MixedTatamis improved

--- PuzzlesM/Puzzles/MixedTatamis.cpp
+++ PuzzlesM/Puzzles/MixedTatamis.cpp
@@ -43,7 +43,8 @@ struct puz_game
     string m_id;
     int m_sidelen;
     map<Position, int> m_pos2num;
-    map<Position, vector<puz_box_info>> m_pos2infos;
+    vector<puz_box_info> m_boxinfos;
+    map<Position, vector<int>> m_pos2infoids;
 
     puz_game(const vector<string>& strs, const xml_node& level);
 };
@@ -70,19 +71,27 @@ puz_game::puz_game(const vector<string>& strs, const xml_node& level)
                     if (!(br.first < m_sidelen && br.second < m_sidelen)) continue;
                     vector<Position> rng;
                     for (int r2 = tl.first; r2 <= br.first; ++r2)
-                        for (int c2 = tl.second; c2 <= br.second; ++c2)
-                            if (Position p(r2, c2); m_pos2num.count(p) == 1)
+                        for (int c2 = tl.second; c2 <= br.second; ++c2) {
+                            Position p(r2, c2);
+                            if (auto it = m_pos2num.find(p); it != m_pos2num.end()) {
                                 rng.push_back(p);
-                    // 3. A cell with a number indicates the length of the Tatami.
-                    // Not all Tatamis have to be marked by a number.
-                    if (int sz = rng.size(); sz == 0 || sz == 1 && m_pos2num.at(rng[0]) == i)
-                        for (int r2 = tl.first; r2 <= br.first; ++r2)
-                            for (int c2 = tl.second; c2 <= br.second; ++c2) {
-                                puz_box_info info;
-                                info.m_ch = i + '0';
-                                info.m_box = {tl, br};
-                                m_pos2infos[{r2, c2}].push_back(info);
+                                // 3. A cell with a number indicates the length of the Tatami.
+                                // Not all Tatamis have to be marked by a number.
+                                if (rng.size() > 1 || it->second != i)
+                                    goto next;
                             }
+                        }
+                    if (rng.size() <= 1) {
+                        int n = m_boxinfos.size();
+                        puz_box_info info;
+                        info.m_ch = i + '0';
+                        info.m_box = {tl, br};
+                        m_boxinfos.push_back(info);
+                        for (int r2 = tl.first; r2 <= br.first; ++r2)
+                            for (int c2 = tl.second; c2 <= br.second; ++c2)
+                                m_pos2infoids[{r2, c2}].push_back(n);
+                    }
+                next:;
                 }
 }
 
@@ -91,16 +100,16 @@ struct puz_state
     puz_state() {}
     puz_state(const puz_game& g);
     int sidelen() const {return m_game->m_sidelen;}
-    char cells(int r, int c) const { return m_cells[r * sidelen() + c]; }
-    char& cells(int r, int c) { return m_cells[r * sidelen() + c]; }
+    char cells(const Position& p) const { return m_cells[p.first * sidelen() + p.second]; }
+    char& cells(const Position& p) { return m_cells[p.first * sidelen() + p.second]; }
     bool is_valid(const Position& p) const {
         return p.first >= 0 && p.first < sidelen() && p.second >= 0 && p.second < sidelen();
     }
     bool operator<(const puz_state& x) const {
         return tie(m_cells, m_matches) < tie(x.m_cells, x.m_matches);
     }
-    bool make_move(const Position& p, int n);
-    void make_move2(const Position& p, int n);
+    bool make_move(int n);
+    void make_move2(int n);
     int find_matches(bool init);
     bool check_four_boxes();
 
@@ -125,14 +134,10 @@ struct puz_state
 };
 
 puz_state::puz_state(const puz_game& g)
-: m_cells(g.m_sidelen * g.m_sidelen, PUZ_SPACE), m_game(&g)
+: m_game(&g)
+, m_cells(g.m_sidelen* g.m_sidelen, PUZ_SPACE)
+, m_matches(g.m_pos2infoids)
 {
-    for (auto& kv : g.m_pos2infos) {
-        auto& box_ids = m_matches[kv.first];
-        box_ids.resize(kv.second.size());
-        boost::iota(box_ids, 0);
-    }
-
     find_matches(true);
 }
     
@@ -154,22 +159,19 @@ int puz_state::find_matches(bool init)
         auto& p = kv.first;
         auto& box_ids = kv.second;
 
-        auto& infos = m_game->m_pos2infos.at(p);
         boost::remove_erase_if(box_ids, [&](int id) {
-            auto& info = infos[id];
+            auto& info = m_game->m_boxinfos[id];
             auto& box = info.m_box;
             char ch = info.m_ch;
             for (int r = box.first.first; r <= box.second.first; ++r)
                 for (int c = box.first.second; c <= box.second.second; ++c) {
-                    if (cells(r, c) != PUZ_SPACE)
+                    Position p(r, c);
+                    if (cells(p) != PUZ_SPACE)
                         return true;
                     // 4. Two Tatamis of the same size must not be orthogonally adjacent.
-                    Position p(r, c);
-                    for (auto& os : offset) {
-                        auto p2 = p + os;
-                        if (is_valid(p2) && cells(p2.first, p2.second) == ch)
+                    for (auto& os : offset)
+                        if (auto p2 = p + os; is_valid(p2) && cells(p2) == ch)
                             return true;
-                    }
                 }
             return false;
         });
@@ -179,22 +181,24 @@ int puz_state::find_matches(bool init)
             case 0:
                 return 0;
             case 1:
-                return make_move2(p, box_ids.front()), 1;
+                return make_move2(box_ids[0]), 1;
             }
     }
     return check_four_boxes() ? 2 : 0;
 }
 
-void puz_state::make_move2(const Position& p, int n)
+void puz_state::make_move2(int n)
 {
-    auto& info = m_game->m_pos2infos.at(p)[n];
+    auto& info = m_game->m_boxinfos[n];
     auto& box = info.m_box;
     char ch = info.m_ch;
 
     auto &tl = box.first, &br = box.second;
     for (int r = tl.first; r <= br.first; ++r)
-        for (int c = tl.second; c <= br.second; ++c)
-            cells(r, c) = ch, ++m_distance, m_matches.erase({r, c});
+        for (int c = tl.second; c <= br.second; ++c) {
+            Position p(r, c);
+            cells(p) = ch, ++m_distance, m_matches.erase(p);
+        }
     for (int r = tl.first; r <= br.first; ++r)
         m_vert_walls.emplace(r, tl.second),
         m_vert_walls.emplace(r, br.second + 1);
@@ -203,10 +207,10 @@ void puz_state::make_move2(const Position& p, int n)
         m_horz_walls.emplace(br.first + 1, c);
 }
 
-bool puz_state::make_move(const Position& p, int n)
+bool puz_state::make_move(int n)
 {
     m_distance = 0;
-    make_move2(p, n);
+    make_move2(n);
     int m;
     while ((m = find_matches(false)) == 1);
     return m == 2;
@@ -221,7 +225,7 @@ void puz_state::gen_children(list<puz_state>& children) const
     });
     for (int n : kv.second) {
         children.push_back(*this);
-        if (!children.back().make_move(kv.first, n))
+        if (!children.back().make_move(n))
             children.pop_back();
     }
 }
