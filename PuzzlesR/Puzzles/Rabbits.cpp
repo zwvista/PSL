7b611f2e456c4bb06f53b60f8c7f02c04359c84b
zwvista <zwvista@msn.com>
2020-11-19 17:11:28 +0900

    Rabbits 5

--- PuzzlesR/Puzzles/Rabbits.cpp
+++ PuzzlesR/Puzzles/Rabbits.cpp
@@ -105,7 +105,9 @@ struct puz_state
     int sidelen() const {return m_game->m_sidelen;}
     char cells(const Position& p) const { return m_cells[p.first * sidelen() + p.second]; }
     char& cells(const Position& p) { return m_cells[p.first * sidelen() + p.second]; }
-    bool operator<(const puz_state& x) const { return m_matches < x.m_matches; }
+    bool operator<(const puz_state& x) const {
+        return tie(m_cells, m_matches) < tie(x.m_cells, x.m_matches);
+    }
     bool make_move(int i, int j);
     void make_move2(int i, int j);
     int find_matches(bool init);
