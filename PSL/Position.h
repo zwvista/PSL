b2cf409701b28ac433993d3f242845596a144bf5
趙偉 <zwvista@msn.com>
2025-05-25 04:00:15 +0900

    position

--- PSL/Position.h
+++ PSL/Position.h
@@ -23,11 +23,6 @@ void parse_position(const string& str, Position& p);
 void parse_positions(const string& str, vector<Position>& vp);
 
 namespace std {
-    inline ostream& operator<<(ostream& out, const Position& p)
-    {
-        out << std::format("{}", p);
-        return out;
-    }
     template<>
     struct formatter<Position> : formatter<string> {
         auto format(Position p, format_context& ctx) const {
@@ -36,4 +31,9 @@ namespace std {
                 ctx);
         }
     };
+    inline ostream& operator<<(ostream& out, const Position& p)
+    {
+        out << std::format("{}", p);
+        return out;
+    }
 }
