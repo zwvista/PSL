502515a9d4531d17d58ce147533ae1b8562fe15c
zwvista <zwvista@msn.com>
2025-06-11 17:37:45 +0900

    Inbetween Nurikabe 1

--- PuzzlesI/main.cpp
+++ PuzzlesI/main.cpp
@@ -1,6 +1,7 @@
 #include "stdafx.h"
 
 extern void solve_puz_icedin();
+extern void solve_puz_InbetweenNurikabe();
 extern void solve_puz_InbetweenSumscrapers();
 extern void solve_puz_InsaneTatamis();
 extern void solve_puz_IslandConnections();
@@ -9,15 +10,17 @@ int main(int argc, char **argv)
 {
     srand(time(0));
     println("c: icedin");
-    println("n1: Inbetween Sumscrapers");
-    println("n2: Insane Tatamis");
+    println("n1: Inbetween Nurikabe");
+    println("n2: Inbetween Sumscrapers");
+    println("n3: Insane Tatamis");
     println("s: Island Connections");
     string str;
     getline(cin, str);
     if (str.empty());
     else if (str == "c") solve_puz_icedin();
-    else if (str == "n1") solve_puz_InbetweenSumscrapers();
-    else if (str == "n2") solve_puz_InsaneTatamis();
+    else if (str == "n1") solve_puz_InbetweenNurikabe();
+    else if (str == "n2") solve_puz_InbetweenSumscrapers();
+    else if (str == "n3") solve_puz_InsaneTatamis();
     else if (str == "s") solve_puz_IslandConnections();
 
     return 0;
