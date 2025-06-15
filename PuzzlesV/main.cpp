b505e3836c3c4f35a264814ba639fd31aa5deb22
zwvista <zwvista@msn.com>
2025-06-16 01:59:37 +0900

    PuzzlesV

---
+++ PuzzlesV/main.cpp
@@ -0,0 +1,15 @@
+#include "stdafx.h"
+
+extern void solve_puz_Venice();
+
+int main(int argc, char **argv)
+{
+    srand(time(0));
+    println("e: Venice");
+    string str;
+    getline(cin, str);
+    if (str.empty());
+    else if (str == "e") solve_puz_Venice();
+
+    return 0;
+}
