81ead20232030eb55aba44d452d45adda70bd829
zwvista <zwvista@msn.com>
2025-06-15 09:00:26 +0900

    YouTurnMeOn 1

--- PuzzlesY/main.cpp
+++ PuzzlesY/main.cpp
@@ -1,15 +1,18 @@
 #include "stdafx.h"
 
 extern void solve_puz_Yalooniq();
+extern void solve_puz_YouTurnMeOn();
 
 int main(int argc, char **argv)
 {
     srand(time(0));
     println("a: Yalooniq");
+    println("o: You Turn me on");
     string str;
     getline(cin, str);
     if (str.empty());
     else if (str == "a") solve_puz_Yalooniq();
+    else if (str == "o") solve_puz_YouTurnMeOn();
 
     return 0;
 }
