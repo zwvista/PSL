bb1ad77b423ebb36d72243127962f7cd8e781ef4
zwvista <zwvista@msn.com>
2025-06-23 13:05:47 +0900

    PuzzlesJ

---
+++ PuzzlesJ/main.cpp
@@ -0,0 +1,15 @@
+#include "stdafx.h"
+
+extern void solve_puz_JoinMe();
+
+int main(int argc, char **argv)
+{
+    srand(time(0));
+    println("o: Join Me!");
+    string str;
+    getline(cin, str);
+    if (str.empty());
+    else if (str == "o") solve_puz_JoinMe();
+
+    return 0;
+}
