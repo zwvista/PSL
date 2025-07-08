919d3f01489f6c3d98ceeca6aac2a0af84304c1f
zwvista <zwvista@msn.com>
2025-07-08 15:38:04 +0900

    ZenSolitaire 9

--- PuzzlesZ/Puzzles/ZenSolitaire.cpp
+++ PuzzlesZ/Puzzles/ZenSolitaire.cpp
@@ -36,6 +36,7 @@ constexpr Position offset[] = {
 
 struct puz_move
 {
+    int m_dir; // direction to the next stone
     Position m_to;
     set<Position> m_on_path; // stones on the path
 };
@@ -62,11 +63,14 @@ puz_game::puz_game(const vector<string>& strs, const xml_node& level)
                 m_stones.emplace(r, c);
     }
 
+    // 3. From a stone, you can move horizontally or vertically to the next stone. You can't
+    // jump over stones, if you encounter it, you have to pick it up.
     for (auto& p1 : m_stones)
         for (auto& [r1, c1] = p1; auto& p2 : m_stones)
             if (p1 < p2)
                 if (auto& [r2, c2] = p2; r1 == r2 || c1 == c2) {
                     set<Position> on_path;
+                    int dir = r1 == r2 ? 1 : 2;
                     if (r1 == r2)
                         for (int c = c1 + 1; c < c2; ++c)
                             if (Position p3(r1, c); m_stones.contains(p3))
@@ -75,8 +79,8 @@ puz_game::puz_game(const vector<string>& strs, const xml_node& level)
                         for (int r = r1 + 1; r < r2; ++c)
                             if (Position p3(r, c1); m_stones.contains(p3))
                                 on_path.insert(p3);
-                    m_stone2moves[p1].emplace_back(p2, on_path);
-                    m_stone2moves[p2].emplace_back(p1, on_path);
+                    m_stone2moves[p1].emplace_back(dir, p2, on_path);
+                    m_stone2moves[p2].emplace_back((dir + 2) % 4, p1, on_path);
                 }
 }
 
@@ -90,6 +94,7 @@ struct puz_state
     bool make_move(const Position& p, int n);
 
     //solve_puzzle interface
+    // 6. The goal is to pick up every stone.
     bool is_goal_state() const { return get_heuristic() == 0; }
     void gen_children(list<puz_state>& children) const;
     unsigned int get_heuristic() const { return m_stone2move_ids.size(); }
@@ -110,7 +115,7 @@ puz_state::puz_state(const puz_game& g)
     for (auto& [p, moves] : g.m_stone2moves) {
         auto& ids = m_stone2move_ids[p];
         for (int i = 0; i < moves.size(); ++i)
-            if (auto& [_1, on_path] = moves[i]; on_path.empty())
+            if (auto& [_1, _2, on_path] = moves[i]; on_path.empty())
                 ids.push_back(i);
     }
 }
@@ -120,7 +125,7 @@ bool puz_state::make_move(const Position& p, int n)
     if (m_path.empty())
         m_path.push_back(p); // first stone picked up
     m_stone2move_ids.erase(p);
-    auto& [to, _1] = m_game->m_stone2moves.at(p)[n];
+    auto& [dir, to, _1] = m_game->m_stone2moves.at(p)[n];
     m_path.push_back(to);
 
     if (m_stone2move_ids.size() == 1)
@@ -129,8 +134,12 @@ bool puz_state::make_move(const Position& p, int n)
         for (auto& [p2, move_ids] : m_stone2move_ids) {
             auto& moves = m_game->m_stone2moves.at(p2);
             for (int i = 0; i < moves.size(); ++i)
-                if (auto& [to2, on_path2] = moves[i]; !m_stone2move_ids.contains(to2))
+                // 4. When moving from a stone to another, you can change direction, but you cannot reverse it.
+                if (auto& [dir2, to2, on_path2] = moves[i];
+                    !m_stone2move_ids.contains(to2) || p2 == to && dir2 == (dir + 2) % 4)
                     boost::remove_erase(move_ids, i);
+                // 5. when a stone has been picked up, you can pass away it if you encounter it again
+                // (it's not there anymore).
                 else if (!on_path2.empty() && boost::algorithm::none_of(on_path2, [&](const Position& p2) {
                     return m_stone2move_ids.contains(p2);
                 }))
@@ -148,6 +157,8 @@ void puz_state::gen_children(list<puz_state>& children) const
             if (children.push_back(*this); !children.back().make_move(p, n))
                 children.pop_back();
     };
+    // 2. You can start at any stone and pick it up (just to click on it and it will be numbered
+    // in the order you pick it up).
     if (m_path.empty())
         for (auto& p : m_game->m_stones)
             f(p);
@@ -163,7 +174,7 @@ ostream& puz_state::dump(ostream& out) const
                 out << ".. ";
             else {
                 int n = boost::find(m_path, p) - m_path.begin() + 1;
-                out << format("{:2}", n) << ' ';
+                out << format("{:2} ", n);
             }
         println(out);
     }
