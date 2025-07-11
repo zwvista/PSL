d637f44be7aa041a74aee6d59990bea2ee1c6852
zwvista <zwvista@msn.com>
2025-07-11 09:53:58 +0900

    Nooks 3

--- PuzzlesN/Puzzles/Nooks.cpp
+++ PuzzlesN/Puzzles/Nooks.cpp
@@ -35,10 +35,17 @@ constexpr Position offset[] = {
     {0, -1},       // w
 };
 
+constexpr Position offset2[] = {
+    {0, 0},        // n
+    {0, 1},        // e
+    {1, 0},        // s
+    {0, 0},        // w
+};
+
 struct puz_move
 {
     int m_dir;
-    set<Position> m_hedge, m_empties;
+    set<Position> m_hedges, m_empties;
 };
 
 struct puz_game
@@ -48,6 +55,7 @@ struct puz_game
     string m_cells;
     map<Position, int> m_pos2num;
     map<Position, vector<puz_move>> m_pos2moves;
+    vector<string> m_perms;
 
     puz_game(const vector<string>& strs, const xml_node& level);
     char cells(const Position& p) const { return m_cells[p.first * m_sidelen + p.second]; }
@@ -90,6 +98,13 @@ puz_game::puz_game(const vector<string>& strs, const xml_node& level)
                 moves.push_back({i, hedges, empties});
         }
     }
+
+    for (int i = 1; i <= 3; ++i) {
+        auto perm = string(i, PUZ_EMPTY) + string(4 - i, PUZ_HEDGE);
+        do
+            m_perms.push_back(perm);
+        while (boost::next_permutation(perm));
+    }
 }
 
 struct puz_state
@@ -99,36 +114,50 @@ struct puz_state
     char cells(const Position& p) const { return m_cells[p.first * sidelen() + p.second]; }
     char& cells(const Position& p) { return m_cells[p.first * sidelen() + p.second]; }
     bool operator<(const puz_state& x) const {
-        return tie(m_cells, m_matches) < tie(x.m_cells, x.m_matches);
+        return tie(m_cells, m_matches_hint) < tie(x.m_cells, x.m_matches_hint);
     }
-    bool make_move(const Position& p, int n);
-    void make_move2(const Position& p, int n);
+    bool make_move_hint(const Position& p, int n);
+    void make_move_hint2(const Position& p, int n);
+    bool make_move_2x2(const Position& p, int n);
     int find_matches(bool init);
     bool is_interconnected() const;
 
     //solve_puzzle interface
     bool is_goal_state() const { return get_heuristic() == 0; }
     void gen_children(list<puz_state>& children) const;
-    unsigned int get_heuristic() const { return m_matches.size(); }
+    unsigned int get_heuristic() const { return m_matches_hint.size(); }
     unsigned int get_distance(const puz_state& child) const { return child.m_distance; }
     void dump_move(ostream& out) const {}
     ostream& dump(ostream& out) const;
 
     const puz_game* m_game = nullptr;
     string m_cells;
-    map<Position, vector<int>> m_matches;
+    map<Position, vector<int>> m_matches_hint;
+    map<Position, vector<int>> m_matches_2x2;
     unsigned int m_distance = 0;
 };
 
 puz_state::puz_state(const puz_game& g)
 : m_game(&g)
 {
+    for (auto& [p, moves] : g.m_pos2moves) {
+        auto& v = m_matches_hint[p];
+        v.resize(moves.size());
+        boost::iota(v, 0);
+    }
+
+    vector<int> v2(g.m_perms.size());
+    boost::iota(v2, 0);
+    for (int r = 1; r < g.m_sidelen - 2; ++r)
+        for (int c = 1; c < g.m_sidelen - 2; ++c)
+            m_matches_2x2[{r, c}] = v2;
+
     find_matches(true);
 }
 
 int puz_state::find_matches(bool init)
 {
-    for (auto& [p, move_ids] : m_matches) {
+    for (auto& [p, move_ids] : m_matches_hint) {
         //boost::remove_erase_if(area_ids, [&](int id) {
         //    auto& [rng, _2, rng2D] = m_game->m_areas[id];
         //    return !boost::algorithm::all_of(rng2D, [&](const set<Position>& rng2) {
@@ -144,7 +173,7 @@ int puz_state::find_matches(bool init)
             case 0:
                 return 0;
             case 1:
-                return make_move2(p, move_ids[0]), 1;
+                return make_move_hint2(p, move_ids[0]), 1;
             }
     }
     return 2;
@@ -183,43 +212,60 @@ bool puz_state::is_interconnected() const
     }) == boost::count_if(m_cells, is_maze);
 }
 
-void puz_state::make_move2(const Position& p, int n)
+void puz_state::make_move_hint2(const Position& p, int n)
 {
+    auto& [_1, hedges, empties] = m_game->m_pos2moves.at(p)[n];
+    for (auto& p2 : hedges)
+        cells(p2) = PUZ_HEDGE;
+    for (auto& p2 : empties)
+        cells(p2) = PUZ_EMPTY;
+    ++m_distance, m_matches_hint.erase(p);
 }
 
-bool puz_state::make_move(const Position& p, int n)
+bool puz_state::make_move_hint(const Position& p, int n)
 {
     m_distance = 0;
-    make_move2(p, n);
+    make_move_hint2(p, n);
     int m;
     while ((m = find_matches(false)) == 1);
     return m == 2;
 }
 
+bool puz_state::make_move_2x2(const Position& p, int n)
+{
+    auto& perm = m_game->m_perms[n];
+    for (int i = 0; i < 4; ++i) {
+        Position p2 = p + offset2[i];
+        if (char& ch = cells(p2); ch == PUZ_EMPTY)
+            ch = perm[i];
+    }
+    return true;
+}
+
 void puz_state::gen_children(list<puz_state>& children) const
 {
-    auto& [p, move_ids] = *boost::min_element(m_matches, [](
+    auto& [p, move_ids] = *boost::min_element(m_matches_hint, [](
         const pair<const Position, vector<int>>& kv1,
         const pair<const Position, vector<int>>& kv2) {
         return kv1.second.size() < kv2.second.size();
     });
     for (int n : move_ids)
-        if (children.push_back(*this); !children.back().make_move(p, n))
+        if (children.push_back(*this); !children.back().make_move_hint(p, n))
             children.pop_back();
 }
 
 ostream& puz_state::dump(ostream& out) const
 {
-    //for (int r = 0; r < sidelen(); ++r) {
-    //    for (int c = 0; c < sidelen(); ++c)
-    //        if (Position p(r, c); !m_game->m_stones.contains(p))
-    //            out << ".. ";
-    //        else {
-    //            int n = boost::find(m_path, p) - m_path.begin() + 1;
-    //            out << format("{:2} ", n);
-    //        }
-    //    println(out);
-    //}
+    for (int r = 1; r < sidelen() - 1; ++r) {
+        for (int c = 1; c < sidelen() - 1; ++c) {
+            Position p(r, c);
+            if (char ch = cells(p); ch == PUZ_NOOK)
+                out << ch << m_game->m_pos2num.at(p);
+            else
+                out << ch << ch;
+        }
+        println(out);
+    }
     return out;
 }
 
