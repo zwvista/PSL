8864ba15f5ee03878cbfac2aa19f30636b326323
趙偉 <zwvista@msn.com>
2025-07-20 19:14:34 +0900

    bloxorz description

--- PuzzlesB/Puzzles/bloxorz.cpp
+++ PuzzlesB/Puzzles/bloxorz.cpp
@@ -5,6 +5,18 @@
 
 /*
     https://bloxorz.org/
+ 
+    Description:
+    1) Tap two finger to show this help.The aim of the game is to get the block to fall into the square hole at the end of each stage. There are 33 stages to complete.
+    2) To move the block around the world, wipe left, right, up and down arrow keys. Be careful not to fall off the edges. The level will be restarted if this happens.
+    3) Bridges and switches are located in many levels. The switches are activated when they are pressed down by the block. You do not need to stay resting on the switch to keep bridges closed.
+    4) There are two types of switches: "Heavy" x-shaped ones and "soft" octagon ones... Soft switches (octagons) are activated when any part of your block presses it. Hard switches (x's) require much more pressure, so your block must be standing on its end to activate them.
+    5) When activated, each switch may behave differently. Some will swap the bridges from open to closed to open each time it is used. Some will create bridges permanently. Green or red colored squares will flash to indicate which bridges are being operated.
+    6) Orange tiles are more fragile than the rest of the land. If your block stands up vertically on an orange tile, the tile will give way and your block will fall through.
+    7) Finally, there is a third type of switch shaped like this: ( ) It teleports your block to different locations, splitting it into two smaller blocks at the same time. These can be controlled individually and will rejoin into a normal block when both are places next to each other.
+    8) You can select which small block to use at any time by click the block. Small blocks can still operate soft switches, but they aren't big enough to activate heavy switches. Also, small blocks cannot go through the exit hole -- only a complete block can finish the stage.
+    9) Remember the passcode for each stage. It is located in the top right corner. You can skip straight back to each stage later on by going to "Load Stage" in the main menu and entering the 6 digit level code.
+
 */
 
 namespace puzzles::bloxorz{
