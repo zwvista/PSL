552cd18b8009c63900f42121f72e8a14e3e869a3
zwvista <zwvista@msn.com>
2025-07-25 11:43:34 +0900

    SquareMergePuzzle 3

--- PuzzlesS/Puzzles/SquareMergePuzzle.cpp
+++ PuzzlesS/Puzzles/SquareMergePuzzle.cpp
@@ -11,6 +11,13 @@ constexpr Position offset[] = {
     {0, -1},       // w
 };
 
+constexpr Position offset2[] = {
+    {0, 0},        // 2*2 nw
+    {0, 1},        // 2*2 ne
+    {1, 0},        // 2*2 sw
+    {1, 1},        // 2*2 se
+};
+
 struct puz_game
 {
     string m_id;
@@ -35,12 +42,11 @@ puz_game::puz_game(const vector<string>& strs, const xml_node& level)
         }
 }
 
-using puz_move = pair<Position, int>;
+using puz_move = pair<Position, bool>;
 
 ostream& operator<<(ostream& out, const puz_move& a)
 {
-    const string_view dirs = "LRUD";
-    out << format("({},{}){}", a.first.first, a.first.second, dirs[a.second]);
+    out << format("({},{}){}", a.first.first, a.first.second, a.second ? 'r' : 'l');
     return out;
 }
 
@@ -80,11 +86,32 @@ unsigned int puz_state::get_heuristic() const
             Position p(r, c);
             ch2rng[cells(p)].push_back(p);
         }
+    for (auto& [ch, rng] : m_game->m_ch2rng) {
+        auto& rng2 = ch2rng[ch];
+        for (int i = 0; i < rng.size(); ++i)
+            h += manhattan_distance(rng[i], rng2[i]);
+    }
     return h;
 }
 
 void puz_state::make_move(const Position& p, bool clockwise)
 {
+    // 0 1
+    // 2 3
+    // counter-clockwise
+    // 1 3
+    // 0 2
+    // clockwise
+    // 2 0
+    // 3 1
+    auto d1 = get_heuristic();
+    auto p1 = p + offset2[1], p2 = p + offset2[2], p3 = p + offset2[3];
+    if (!clockwise)
+        cells(p1) = exchange(cells(p3), exchange(cells(p2), exchange(cells(p), cells(p1))));
+    else
+        cells(p2) = exchange(cells(p3), exchange(cells(p1), exchange(cells(p), cells(p2))));
+    m_move = puz_move(p, clockwise);
+    m_distance = d1 - get_heuristic();
 }
 
 void puz_state::gen_children(list<puz_state>& children) const
@@ -116,6 +143,6 @@ ostream& puz_state::dump(ostream& out) const
 void solve_puz_SquareMergePuzzle()
 {
     using namespace puzzles::SquareMergePuzzle;
-    solve_puzzle<puz_game, puz_state, puz_solver_astar<puz_state>>(
+    solve_puzzle<puz_game, puz_state, puz_solver_astar<puz_state, false>>(
         "Puzzles/SquareMergePuzzle.xml", "Puzzles/SquareMergePuzzle.txt");
 }
