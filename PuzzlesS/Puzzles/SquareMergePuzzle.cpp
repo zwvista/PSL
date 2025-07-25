c0d1d143dc51290529461e7e01cf4731c3a631f1
zwvista <zwvista@msn.com>
2025-07-25 11:02:16 +0900

    SquareMergePuzzle 2

--- PuzzlesS/Puzzles/SquareMergePuzzle.cpp
+++ PuzzlesS/Puzzles/SquareMergePuzzle.cpp
@@ -4,67 +4,35 @@
 
 namespace puzzles::SquareMergePuzzle{
 
-enum EBrickType {bt1X1, bt2X1, bt1X2, bt2X2};
-
 constexpr Position offset[] = {
-    {0, -1},
-    {0, 1},
-    {-1, 0},
-    {1, 0},
+    {-1, 0},       // n
+    {0, 1},        // e
+    {1, 0},        // s
+    {0, -1},       // w
 };
-
-using brick_map = map<Position, EBrickType>;
-using brick_pair = pair<const Position, EBrickType>;
-
-using brick_info = vector<Position>;
 
 struct puz_game
 {
-    array<brick_info, 4> m_bricks_info;
-    Position m_size;
-    brick_map m_brick_map;
-    vector<char> m_cells;
-    Position m_caocao, m_goal;
     string m_id;
+    int m_sidelen;
+    string m_cells, m_goal;
+    map<char, vector<Position>> m_ch2rng;
 
     puz_game(const vector<string>& strs, const xml_node& level);
-    int rows() const {return m_size.first;}
-    int cols() const {return m_size.second;}
+    char goal(const Position& p) const { return m_goal[p.first * m_sidelen + p.second]; }
 };
 
 puz_game::puz_game(const vector<string>& strs, const xml_node& level)
     : m_id(level.attribute("id").value())
-    , m_size(strs.size(), strs[0].length())
-    , m_goal(rows() - 3, 2)
+    , m_sidelen(strs.size() / 2)
 {
-    for (int r = 0; r < rows(); ++r) {
-        string_view str = strs[r];
-        for (int c = 0; c < cols(); ++c) {
+    m_goal = accumulate(strs.begin(), strs.begin() + m_sidelen, string());
+    m_cells = accumulate(strs.begin() + m_sidelen, strs.end(), string());
+    for (int r = 0; r < m_sidelen; ++r)
+        for (int c = 0; c < m_sidelen; ++c) {
             Position p(r, c);
-            m_cells.push_back(str[c]);
-            switch(str[c]) {
-            case 'A':
-                m_brick_map[p] = bt1X1;
-                break;
-            case 'B':
-                m_brick_map[p] = bt2X1;
-                break;
-            case 'C':
-                m_brick_map[p] = bt1X2;
-                break;
-            case 'D':
-                m_brick_map[m_caocao = p] = bt2X2;
-                break;
-            }
+            m_ch2rng[goal(p)].push_back(p);
         }
-    }
-
-    Position pos[] = {Position(0, 0), Position(1, 0), Position(0, 1), Position(1, 1)};
-    m_bricks_info[0].push_back(pos[0]);
-    m_bricks_info[1].assign(pos, pos + 2);
-    m_bricks_info[2].push_back(pos[0]);
-    m_bricks_info[2].push_back(pos[2]);
-    m_bricks_info[3].assign(pos, pos + 4);
 }
 
 using puz_move = pair<Position, int>;
@@ -79,76 +47,55 @@ ostream& operator<<(ostream& out, const puz_move& a)
 struct puz_state
 {
     puz_state(const puz_game& g)
-        : m_game(&g), m_cells(g.m_cells), m_caocao(g.m_caocao),
-        m_brick_map(g.m_brick_map) {}
-    int rows() const {return m_game->rows();}
-    int cols() const {return m_game->cols();}
-    char cells(int r, int c) const {return m_cells[r * cols() + c];}
-    char& cells(const Position& p) {return m_cells[p.first * cols() + p.second];}
+        : m_game(&g), m_cells(g.m_cells) {}
+    int sidelen() const { return m_game->m_sidelen; }
+    char cells(const Position& p) const { return m_cells[p.first * sidelen() + p.second]; }
+    char& cells(const Position& p) { return m_cells[p.first * sidelen() + p.second]; }
     bool operator<(const puz_state& x) const {
-        return m_cells < x.m_cells || m_cells == x.m_cells && m_move < x.m_move;
+        return tie(m_cells, m_move) < tie(x.m_cells, x.m_move);
     }
-    bool operator==(const puz_state& x) const {
-        return m_cells == x.m_cells && m_move == x.m_move;
-    }
+    void make_move(const Position& p, bool clockwise);
 
     // solve_puzzle interface
-    bool is_goal_state() const {return m_caocao == m_game->m_goal;}
+    bool is_goal_state() const {return get_heuristic() == 0;}
     void gen_children(list<puz_state>& children) const;
-    unsigned int get_heuristic() const {return manhattan_distance(m_caocao, m_game->m_goal);}
-    unsigned int get_distance(const puz_state& child) const{
-        Position lastto = m_move ? m_move->first + offset[m_move->second] : Position(0, 0);
-        unsigned int d = 1;
-        if (lastto != child.m_move->first)
-            d += 1 << 16;
-        return d;
-    }
+    unsigned int get_heuristic() const;
+    unsigned int get_distance(const puz_state& child) const { return m_distance; }
     //unsigned int get_distance(const puz_state& child) const {return 1;}
     void dump_move(ostream& out) const {if(m_move) out << *m_move;}
     ostream& dump(ostream& out) const;
 
-    bool make_move(const Position& pos, EBrickType bt, const brick_info& bi, int i);
     const puz_game* m_game;
-    vector<char> m_cells;
-    brick_map m_brick_map;
-    Position m_caocao;
-    boost::optional<puz_move> m_move;
+    string m_cells;
+    optional<puz_move> m_move;
+    unsigned int m_distance = 0;
 };
 
-bool puz_state::make_move(const Position& p, EBrickType bt, const brick_info& bi, int i)
+unsigned int puz_state::get_heuristic() const
 {
-    vector<char> vch;
-    for (const Position& u : bi) {
-        char& ch = cells(p + u);
-        vch.push_back(ch);
-        ch = ' ';
-    }
+    unsigned int h = 0;
+    map<char, vector<Position>> ch2rng;
+    for (int r = 0; r < sidelen(); ++r)
+        for (int c = 0; c < sidelen(); ++c) {
+            Position p(r, c);
+            ch2rng[cells(p)].push_back(p);
+        }
+    return h;
+}
 
-    Position p2 = p + offset[i];
-    for (const Position& u : bi)
-        if (cells(p2 + u) != ' ')
-            return false;
-
-    for (size_t j = 0; j < vch.size(); ++j)
-        cells(p2 + bi[j]) = vch[j];
-    m_brick_map.erase(p);
-    m_move = puz_move(p, i);
-    m_brick_map[p2] = bt;
-    if (m_caocao == p)
-        m_caocao = p2;
-    return true;
+void puz_state::make_move(const Position& p, bool clockwise)
+{
 }
 
 void puz_state::gen_children(list<puz_state>& children) const
 {
-    const array<brick_info, 4>& bis = m_game->m_bricks_info;
-    for (const brick_pair& bp : m_brick_map) {
-        const Position& p = bp.first;
-        EBrickType bt = bp.second;
-        const brick_info& bi = bis[bt];
-        for (int i = 0; i < 4; ++i)
-            if (children.push_back(*this); !children.back().make_move(p, bt, bi, i))
-                children.pop_back();
+    for (int r = 0; r < sidelen(); ++r)
+        for (int c = 0; c < sidelen(); ++c) {
+            Position p(r, c);
+            for (int i = 0; i < 2; ++i) {
+                children.push_back(*this);
+                children.back().make_move(p, i == 1);
+            }
     }
 }
 
@@ -156,9 +103,9 @@ ostream& puz_state::dump(ostream& out) const
 {
     if (m_move)
         out << "move: " << *m_move << endl;
-    for (int r = 0; r < rows(); ++r) {
-        for (int c = 0; c < cols(); ++c)
-            out << cells(r, c);
+    for (int r = 0; r < sidelen(); ++r) {
+        for (int c = 0; c < sidelen(); ++c)
+            out << cells({r, c});
         println(out);
     }
     return out;
