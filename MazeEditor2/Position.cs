e9d68580b808c31159a1c608e33811bd4a2314e0
zwvista <zwvista@msn.com>
2025-07-31 17:44:09 +0900

    MazeEditor2 2

---
+++ MazeEditor2/Position.cs
@@ -0,0 +1,40 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace MazeEditor2
+{
+    public readonly record struct Position(int Row, int Col)
+        : IComparable<Position>
+    {
+        public Position() : this(0, 0) { }
+
+        public static Position operator +(Position a, Position b) =>
+            new Position(a.Row + b.Row, a.Col + b.Col);
+
+        public static Position operator -(Position a, Position b) =>
+            new Position(a.Row - b.Row, a.Col - b.Col);
+
+        public int CompareTo(Position other)
+        {
+            int rowCmp = Row.CompareTo(other.Row);
+            if (rowCmp != 0) return rowCmp;
+            return Col.CompareTo(other.Col);
+        }
+
+        public static bool operator <(Position a, Position b) =>
+            a.Row < b.Row || (a.Row == b.Row && a.Col < b.Col);
+
+        public static bool operator >(Position a, Position b) =>
+            a.Row > b.Row || (a.Row == b.Row && a.Col > b.Col);
+
+        public static bool operator <=(Position a, Position b) =>
+            a < b || a == b;
+
+        public static bool operator >=(Position a, Position b) =>
+            a > b || a == b;
+
+    }
+}
