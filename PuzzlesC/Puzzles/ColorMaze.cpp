992c3f9a08f9e4b4442d73047a1f694f0908e918
zwvista <zwvista@msn.com>
2025-08-21 15:20:35 +0900

    ColorMaze 2

--- PuzzlesC/Puzzles/ColorMaze.cpp
+++ PuzzlesC/Puzzles/ColorMaze.cpp
@@ -18,25 +18,25 @@
 namespace puzzles::ColorMaze{
 
 constexpr auto PUZ_SPACE = ' ';
-constexpr auto PUZ_EMPTY = '.';
+constexpr auto PUZ_PAINTED = '.';
+constexpr auto PUZ_BALL = 'O';
+constexpr auto PUZ_BLOCK = '#';
 
-struct puz_tube : vector<string>
-{
-    int water_size() const {
-        return boost::accumulate(*this, 0, [](int sum, const string& s) {
-            return sum + s.length();
-        });
-    }
-    string to_string() const {
-        return boost::accumulate(*this, string());
-    }
+constexpr Position offset[] = {
+    {-1, 0},       // n
+    {0, 1},        // e
+    {1, 0},        // s
+    {0, -1},       // w
 };
+
+const string_view dirs = "^>v<";
 
 struct puz_game
 {
     string m_id;
     Position m_size;
-    vector<puz_tube> m_tubes;
+    Position m_ball;
+    string m_cells;
 
     puz_game(const vector<string>& strs, const xml_node& level);
     int rows() const { return m_size.first; }
@@ -47,19 +47,27 @@ struct puz_move : pair<int, int> { using pair::pair; };
 
 puz_game::puz_game(const vector<string>& strs, const xml_node& level)
     : m_id(level.attribute("id").value())
-    , m_size(Position(strs.size(), strs[0].length()))
-    , m_tubes(cols())
+    , m_size(Position(strs.size() + 2, strs[0].length() + 2))
 {
-    for (int r = 0; r < rows(); ++r) {
-        string_view str = strs[r];
-        for (int c = 0; c < cols(); ++c)
-            if (char ch = str[c]; ch == PUZ_SPACE)
-                ;
-            else if (m_tubes[c].empty() || m_tubes[c].back().back() != ch)
-                m_tubes[c].push_back(string(1, ch));
-            else
-                m_tubes[c].back().push_back(ch);
+    m_cells.append(cols(), PUZ_BLOCK);
+    for (int r = 1; r < rows() - 1; ++r) {
+        string_view str = strs[r - 1];
+        m_cells.push_back(PUZ_BLOCK);
+        for (int c = 1; c < cols() - 1; ++c) {
+            switch (Position p(r, c); char ch = str[c - 1]) {
+            case PUZ_SPACE:
+            case PUZ_BLOCK:
+                m_cells.push_back(ch);
+                break;
+            case PUZ_BALL:
+                m_cells.push_back(PUZ_PAINTED);
+                m_ball = p;
+                break;
+            }
+        }
+        m_cells.push_back(PUZ_BLOCK);
     }
+    m_cells.append(cols(), PUZ_BLOCK);
 }
 
 struct puz_state
@@ -67,83 +75,47 @@ struct puz_state
     puz_state(const puz_game& g);
     int rows() const { return m_game->rows(); }
     int cols() const { return m_game->cols(); }
-    bool operator<(const puz_state& x) const { return m_tubes < x.m_tubes; }
-    void make_move(int i, int j);
-    bool is_finished(const puz_tube& tube) const { return tube.size() == 1 && tube[0].size() == rows(); }
+    bool operator<(const puz_state& x) const {
+        return tie(m_cells, m_ball) < tie(x.m_cells, m_ball); 
+    }
+    void make_move(int n);
 
     //solve_puzzle interface
-    bool is_goal_state() const {
-        return boost::algorithm::all_of(m_tubes, [&](const puz_tube& tube) {
-            return tube.empty() || is_finished(tube);
-        });
-    }
+    bool is_goal_state() const { return get_heuristic() == 0; }
     void gen_children(list<puz_state>& children) const;
-    unsigned int get_heuristic() const {
-        return boost::accumulate(m_tubes, 0, [&](int acc, const puz_tube& tube) {
-            return acc + (is_finished(tube) ? 0 : tube.size()) + (tube.size() > 1 ? 1 : 0);
-        });
-    }
+    unsigned int get_heuristic() const { return boost::count(m_cells, PUZ_SPACE); }
     unsigned int get_distance(const puz_state& child) const { return 1; }
-    void dump_move(ostream& out) const {}
+    void dump_move(ostream& out) const { if (m_move) out << *m_move; }
     ostream& dump(ostream& out) const;
 
     const puz_game* m_game;
-    vector<puz_tube> m_tubes;
-    optional<puz_move> m_move;
+    Position m_ball;
+    string m_cells;
+    optional<char> m_move;
 };
 
 puz_state::puz_state(const puz_game& g)
-    : m_game(&g), m_tubes(g.m_tubes)
+    : m_game(&g)
 {
 }
 
-void puz_state::make_move(int i, int j)
+void puz_state::make_move(int n)
 {
-    auto &tube1 = m_tubes[i], &tube2 = m_tubes[j];
-    if (auto it = tube1.begin(); tube2.empty()) {
-        tube2.push_back(*it);
-        tube1.erase(it);
-    } else if (int n = rows() - tube2.water_size(); it->size() <= n) {
-        tube2.front().append(*it);
-        tube1.erase(it);
-    } else {
-        tube2.front().append(it->substr(0, n));
-        it->erase(0, n);
-    }
-    m_move = puz_move(i, j);
 }
 
 void puz_state::gen_children(list<puz_state>& children) const
 {
-    for (int i = 0; i < m_tubes.size(); i++) {
-        auto& tube1 = m_tubes[i];
-        if (tube1.empty() || is_finished(tube1)) continue; // cannot pour from an empty tube or a finished tube
-        for (int j = 0; j < m_tubes.size(); j++) {
-            auto& tube2 = m_tubes[j];
-            if (i == j || tube2.water_size() == rows()) continue;
-            if (!tube2.empty() && tube1.front().back() != tube2.front().back()) continue; // cannot pour if colors do not match
-            children.push_back(*this);
-            children.back().make_move(i, j);
-        }
+    for (int i = 0; i < 4; i++) {
+        children.push_back(*this);
+        children.back().make_move(i);
     }
 }
 
 ostream& puz_state::dump(ostream& out) const
 {
-    if (m_move) {
-        auto& [i, j] = *m_move;
-        out << format("move: {} -> {}\n", to_string(i + 1), to_string(j + 1));
-    }
-    vector<string> strs;
-    for (int c = 0; c < cols(); c++) {
-        auto str = m_tubes[c].to_string();
-        strs.push_back(string(rows() - str.size(), PUZ_EMPTY) + str);
-    }
-    for (int r = 0; r < rows(); ++r) {
-        for (int c = 0; c < cols(); ++c)
-            out << strs[c][r] << PUZ_SPACE;
-        println(out);
-    }
+    if (m_move)
+        out << "move: " << *m_move << endl;
+    println(out);
     return out;
 }
 
@@ -153,5 +125,5 @@ void solve_puz_ColorMaze()
 {
     using namespace puzzles::ColorMaze;
     solve_puzzle<puz_game, puz_state, puz_solver_astar<puz_state>>(
-        "Puzzles/ColorMaze.xml", "Puzzles/ColorMaze.txt");
+        "Puzzles/ColorMaze.xml", "Puzzles/ColorMaze.txt", solution_format::MOVES_ONLY_SINGLE_LINE);
 }
