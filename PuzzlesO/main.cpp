6db9b3ecc6d6931e094fc845c344ace011f7bee1
趙偉 <zwvista@msn.com>
2025-10-09 14:57:31 +0900

    tests

--- PuzzlesO/main.cpp
+++ PuzzlesO/main.cpp
@@ -22,7 +22,7 @@ int main(int argc, char **argv)
         println("r: Orchards");
         println("v: Over Under");
         getline(cin, str);
-        if (str.empty()) solve_puz_OddsAreEven();
+        if (str.empty()) solve_puz_Orchards();
         else if (str == "d") solve_puz_OddsAreEven();
         else if (str == "n1") solve_puz_on_the_edge();
         else if (str == "n2") solve_puz_OneUpOrDown();
