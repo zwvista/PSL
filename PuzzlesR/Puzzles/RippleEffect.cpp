1327eaece030f3700f04a76ff0050d5c8827d01c
趙偉 <zwvista@msn.com>
2026-03-02 10:49:59 +0900

    RippleEffect 6

--- PuzzlesR/Puzzles/RippleEffect.cpp
+++ PuzzlesR/Puzzles/RippleEffect.cpp
@@ -217,7 +217,7 @@ ostream& puz_state::dump(ostream& out) const
     for (int r = 0;; ++r) {
         // draw horizontal lines
         for (int c = 0; c < sidelen(); ++c)
-            out << (m_game->m_horz_walls.contains({r, c}) ? " -" : "  ");
+            out << (m_game->m_horz_walls.contains({r, c}) ? " --" : "   ");
         println(out);
         if (r == sidelen()) break;
         for (int c = 0;; ++c) {
@@ -225,7 +225,7 @@ ostream& puz_state::dump(ostream& out) const
             // draw vertical lines
             out << (m_game->m_vert_walls.contains(p) ? '|' : ' ');
             if (c == sidelen()) break;
-            out << cells(p);
+            out << format("{:<2}", cells(p));
         }
         println(out);
     }
