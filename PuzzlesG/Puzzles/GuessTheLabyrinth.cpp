dac59262cbbbe3ce876e5cd4382f50da7db5deef
趙偉 <zwvista@msn.com>
2026-05-08 16:09:26 +0900

    GuessTheLabyrinth 4

--- PuzzlesG/Puzzles/GuessTheLabyrinth.cpp
+++ PuzzlesG/Puzzles/GuessTheLabyrinth.cpp
@@ -36,7 +36,14 @@ const vector<int> linesegs_all_post = {
     11, 7, 14, 13,
 };
 
-constexpr array<Position, 8> offset = Position::Directions8;
+constexpr array<Position, 4> offset = Position::Directions4;
+
+constexpr Position offset2[] = {
+    {0, 0},        // o
+    {0, 1},        // e
+    {1, 1},        // se
+    {1, 0},        // s
+};
 
 struct puz_game
 {
@@ -44,6 +51,7 @@ struct puz_game
     Position m_size;
     int m_dot_count;
     set<Position> m_posts;
+    set<Position> m_all_positions;
 
     puz_game(const vector<string>& strs, const xml_node& level);
     int rows() const { return m_size.first; }
@@ -52,7 +60,7 @@ struct puz_game
 
 puz_game::puz_game(const vector<string>& strs, const xml_node& level)
 : m_id(level.attribute("id").value())
-, m_size(strs.size() / 2, strs[0].length() / 2)
+, m_size(strs.size() / 2 + 1, strs[0].length() / 2 + 1)
 , m_dot_count(rows() * cols())
 {
     for (int r = 0; r < rows(); ++r) {
@@ -62,6 +70,9 @@ puz_game::puz_game(const vector<string>& strs, const xml_node& level)
             if (str_h[c * 2] == PUZ_POST)
                 m_posts.emplace(r, c);
     }
+    for (int r = 0; r < rows() - 1; ++r)
+        for (int c = 0; c < cols() - 1; ++c)
+            m_all_positions.emplace(r, c);
 }
 
 using puz_dot = vector<int>;
@@ -79,11 +90,9 @@ struct puz_state
     bool operator<(const puz_state& x) const {
         return tie(m_matches, m_dots) < tie(x.m_matches, x.m_dots);
     }
-    bool make_move_hint(const Position& p, int n);
-    void make_move_hint2(const Position& p, int n);
     bool make_move_dot(const Position& p, int n);
     int check_dots(bool init);
-    bool check_loop() const;
+    bool check_route() const;
 
     //solve_puzzle interface
     bool is_goal_state() const { return get_heuristic() == 0; }
@@ -103,20 +112,22 @@ struct puz_state
 };
 
 puz_state::puz_state(const puz_game& g)
-: m_dots(g.m_dot_count, {lineseg_off}), m_game(&g)
+: m_dots(g.m_dot_count), m_game(&g)
 {
     for (int r = 0; r < rows(); ++r)
         for (int c = 0; c < cols(); ++c) {
             Position p(r, c);
 
             auto& dt = dots(p);
+            if (r > 0 && c > 0 && r < rows() - 1 && c < cols() - 1)
+                dt.push_back(lineseg_off);
             auto& linesegs_all = m_game->m_posts.contains(p) ? linesegs_all_post : linesegs_all_two;
             for (int lineseg : linesegs_all)
                 if ([&]{
                     for (int i = 0; i < 4; ++i) {
                         if (!is_lineseg_on(lineseg, i))
                             continue;
-                        auto p2 = p + offset[i * 2];
+                        auto p2 = p + offset[i];
                         // A line segment cannot go beyond the boundaries of the board
                         // or cover any number cell
                         if (!is_valid(p2))
@@ -159,7 +170,7 @@ int puz_state::check_dots(bool init)
             if (dt.empty())
                 return 0;
             int lineseg = dt[0];
-            auto p2 = p + offset[i * 2];
+            auto p2 = p + offset[i];
             if (is_valid(p2)) {
                 auto& dt2 = dots(p2);
                 // The line segments in adjacent cells must be connected
@@ -175,39 +186,54 @@ int puz_state::check_dots(bool init)
     }
 }
 
-bool puz_state::check_loop() const
+bool puz_state::check_route() const
 {
     set<Position> rng;
-    for (int r = 0; r < rows(); ++r)
-        for (int c = 0; c < cols(); ++c) {
-            Position p(r, c);
-            auto& dt = dots(p);
-            if (dt.size() == 1 && dt[0] != lineseg_off)
-                rng.insert(p);
+    for (auto& p : m_game->m_all_positions) {
+        int num = 2;
+        int max_possible = 0;
+        int min_guaranteed = 0;
+        for (int i = 0; i < 4; ++i) {
+            auto& dt = dots(p + offset2[i]);
+            int d = (i + 1) % 4;
+            if (boost::algorithm::any_of(dt, [&](int lineseg) {
+                return is_lineseg_on(lineseg, d);
+            })) max_possible++;
+            if (boost::algorithm::all_of(dt, [&](int lineseg) {
+                return is_lineseg_on(lineseg, d);
+            })) min_guaranteed++;
         }
-
-    bool has_branch = false;
-    while (!rng.empty()) {
-        auto p = *rng.begin(), p2 = p;
-        for (int n = -1;;) {
-            rng.erase(p2);
-            auto& lineseg = dots(p2)[0];
-            for (int i = 0; i < 4; ++i)
-                // proceed only if the line segment does not revisit the previous position
-                if (is_lineseg_on(lineseg, i) && (i + 2) % 4 != n) {
-                    p2 += offset[(n = i) * 2];
-                    break;
-                }
-            if (p2 == p)
-                // we have a loop here,
-                // and we are supposed to have exhausted the line segments
-                return !has_branch && rng.empty();
-            if (!rng.contains(p2)) {
-                has_branch = true;
-                break;
-            }
-        }
+        // Prune if we can't possibly reach the target
+        if (max_possible < num) return false;
+        // Prune if we have already exceeded the target
+        if (min_guaranteed > num) return false;
+        // Final check
+        if (is_goal_state() && min_guaranteed != num) return false;
+        if (min_guaranteed == num)
+            rng.insert(p);
     }
+
+//    bool has_branch = false;
+//    while (!rng.empty()) {
+//        auto p = *rng.begin(), p2 = p;
+//        for (int n = -1;;) {
+//            rng.erase(p2);
+//            for (int i = 0; i < 4; ++i)
+//                // proceed only if the route does not revisit the previous position
+//                if (is_lineseg_on(lineseg, i) && (i + 2) % 4 != n) {
+//                    p2 += offset[n = i];
+//                    break;
+//                }
+//            if (p2 == p)
+//                // we have a loop here,
+//                // and we are supposed to have exhausted the line segments
+//                return !has_branch && rng.empty();
+//            if (!rng.contains(p2)) {
+//                has_branch = true;
+//                break;
+//            }
+//        }
+//    }
     return true;
 }
 
@@ -217,7 +243,7 @@ bool puz_state::make_move_dot(const Position& p, int n)
     auto& dt = dots(p);
     dt = {dt[n]};
     int m = check_dots(false);
-    return m == 1 ? check_loop() : m == 2;
+    return m == 1 ? check_route() : m == 2;
 }
 
 void puz_state::gen_children(list<puz_state>& children) const
@@ -242,7 +268,8 @@ ostream& puz_state::dump(ostream& out) const
         // draw horizontal lines
         for (int c = 0; c < cols(); ++c) {
             Position p(r, c);
-            out << ' ' << (is_lineseg_on(dots(p)[0], 1) ? '-' : ' ');
+            out << (m_game->m_posts.contains(p) ? PUZ_POST : ' ')
+                << (is_lineseg_on(dots(p)[0], 1) ? '-' : ' ');
         }
         println(out);
         if (r == rows() - 1) break;
